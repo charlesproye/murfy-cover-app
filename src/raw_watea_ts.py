d2b19fc73b410f04641f6fe22f5e2d8e3a26ca7d
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-07-05 15:50:16 +0200

    Second dummy commit ot test pll request

---
+++ src/raw_watea_ts.py
@@ -0,0 +1,14 @@
+"""
+This module provides the function `raw_time_series_of` to provide a raw time series from Tesla's personal API.
+Can also be used as a script to obtain the entirety of the tesla fleet data.
+"""
+import pandas as pd
+from pandas import DataFrame as DF
+
+from watea_constants import *
+
+# Dummy comment to test pull request
+# Second Dummy comment to test pull request
+
+def raw_ts_of(id: str) -> DF:
+    return pd.read_parquet(PATH_TO_RAW_TS.format(id=id))
