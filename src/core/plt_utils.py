Fix: charge and discharge masking from soc (againstatusstatusFeat: Added hlines functionality. Changed fleet_info's columns X_not_na_percentage to just 'has_power_during_charge' and 'has_power_during_discharge'

src/core/plt_utils.py
@@ -45,7 +45,7 @@ def setup_fig_axs_and_layouts(plt_layout:dict, title=None) -> tuple[Figure, np.n
     nb_rows = len(ts_cols) + sum([len(perf_cols) for _, perf_cols in perfs_cols.items()])
     fig: Figure
     axs: np.ndarray[Axes]
-    fig, axs = plt.subplots(nrows=nb_rows, sharex=True)
+    fig, axs = plt.subplots(nrows=nb_rows, sharex=True, squeeze=True)
 
     if title:
         fig.suptitle(title)
@@ -89,7 +89,12 @@ def fill_ax(ax: Axes, df:DF, x:str, y:str|dict, plt_kwargs:dict=DEFAULT_LINE_PLO
         assert "y" in y, "Passed dict to plot Axes but there is no column 'y' in that dict."
         plt_kwargs = {key: val for key, val in y.items() if key != "y"}
         y = y["y"]
-    if is_bool_dtype(df[y]):
+    if plt_kwargs.get("kind", "line") == "hlines":
+        plt_kwargs = {key: val for key, val in plt_kwargs.items() if key != "kind"}
+        xmin, xmax = ax.get_xlim()
+        ax.hlines(y, xmin, xmax, **plt_kwargs)
+        ax.set_xlim(xmin, xmax)
+    elif is_bool_dtype(df[y]):
         ax_min, ax_max = ax.get_ylim()
         ax.fill_between(df.index, ax_min, ax_max, df[y].values, color=plt_kwargs.get("color", "green"), alpha=plt_kwargs.get("alpha", 0.6), label=y)
         ax.set_ylim(ax_min, ax_max)

src/core/time_series_processing.py
@@ -65,7 +65,6 @@ def all_charge_and_discharge_cols_from_soc_diff(vheicle_df: DF) -> DF:
     return (
         vheicle_df
         .pipe(in_discharge_and_charge_from_soc_diff)
-        .pipe(in_discharge_and_charge_from_soc_diff)
         .pipe(perf_mask_and_idx_from_condition_mask, "in_discharge")
         .pipe(perf_mask_and_idx_from_condition_mask, "in_charge")
     )
@@ -86,8 +85,8 @@ def in_discharge_and_charge_from_soc_diff(vehicle_df: DF) -> DF:
     vehicle_df["smoothed_soc_dir"] = vehicle_df["soc_dir"].rolling(window=TD(minutes=20), center=True).mean()
     vehicle_df["soc_dir"] = (
         vehicle_df["soc_dir"]
-        .mask(vehicle_df["smoothed_soc_dir"].gt(0), 1)
-        .mask(vehicle_df["smoothed_soc_dir"].lt(0), -1)
+        .mask(vehicle_df["smoothed_soc_dir"].gt(0, fill_value=False) & vehicle_df["soc_dir"].lt(0, fill_value=False), np.nan)
+        .mask(vehicle_df["smoothed_soc_dir"].lt(0, fill_value=False) & vehicle_df["soc_dir"].gt(0, fill_value=False), np.nan)
     )
 
     bfilled_dir = vehicle_df["soc_dir"].bfill()
@@ -103,11 +102,12 @@ def in_discharge_and_charge_from_soc_diff(vehicle_df: DF) -> DF:
 
 
 # TODO: Find why some perfs grps have a size of 1 even though they are supposed to be filtered out with  trimed_series if trimed_series.sum() > 1 else False
-def perf_mask_and_idx_from_condition_mask(vehicle_df: DF, src_mask:str) -> DF:
+def perf_mask_and_idx_from_condition_mask(vehicle_df: DF, src_mask:str, check_for_monotonicity=False) -> DF:
     src_mask_idx_col_name = f"{src_mask}_idx"
     perf_mask = f"{src_mask}_perf_mask"
     vehicle_df[src_mask_idx_col_name] = period_idx_of_mask(vehicle_df[src_mask])
-    vehicle_df[perf_mask] = vehicle_df.groupby(src_mask_idx_col_name)["soc"].transform(sanitize_perf_period) & vehicle_df[src_mask]
+    perf_grps = vehicle_df.groupby(src_mask_idx_col_name)["soc"]
+    vehicle_df[perf_mask] = perf_grps.transform(sanitize_perf_period) & vehicle_df[src_mask]
     vehicle_df[f"{src_mask}_perf_idx"] = period_idx_of_mask(vehicle_df[perf_mask])
 
     return vehicle_df

src/watea/watea_perfs.py
@@ -1,5 +1,6 @@
 from pandas import DataFrame as DF
 from rich import print
+import numpy as np
 
 from watea.watea_constants import *
 from core.argparse_utils import parse_kwargs
@@ -13,8 +14,6 @@ def main():
 
 def compute_perfs(vehicle_df: DF) -> dict[str, DF]:        
     return {
-        "motion_perfs": perfs.motion_perfs_df_of(vehicle_df, FORD_ETRANSIT_DEFAULT_KWH_PER_SOC, cum_energy_spent_col="cum_energy"),
-        # "self_discharge_perfs": perfs.self_discharge_df_of(vehicle_df, FORD_ETRANSIT_DEFAULT_KWH_PER_SOC, cum_energy_spent_col="cum_energy"),
         "discharge_perfs": compute_discharge_perfs(vehicle_df, FORD_ETRANSIT_DEFAULT_KWH_PER_SOC),
         "charge_perfs": compute_charging_perfs(vehicle_df, FORD_ETRANSIT_DEFAULT_KWH_PER_SOC),
     }
@@ -28,7 +27,9 @@ def compute_discharge_perfs(vehicle_df:DF, default_kwh_per_soc:float) -> DF:
             {"cum_energy": "energy_diff"}
         )
         .pipe(perfs.compute_soh_from_soc_and_energy_diff, "energy_diff", default_kwh_per_soc, "discharge_soh")
+        .pipe(lambda df: df.assign(discharge_soh=df["discharge_soh"].replace(0, np.nan)))
         .eval("km_per_soc = distance / soc_diff")
+        .pipe(lambda df: df.assign(km_per_soc=df["km_per_soc"].replace(0, np.nan)))
     ) 
 
 
@@ -36,6 +37,7 @@ def compute_charging_perfs(vehicle_df: DF, default_kwh_per_soc:float) -> DF:
     return (
         perfs.agg_diffs_df_of(vehicle_df, "in_charge_perf_mask", "in_charge_perf_idx", {"cum_energy": "energy_added", "battery_range_km": "range_gained"})
         .pipe(perfs.compute_soh_from_soc_and_energy_diff, "energy_added", default_kwh_per_soc, "energy_soh")
+        .pipe(lambda df: df.assign(energy_soh=df["energy_soh"].replace(0, np.nan)))
         .eval("battery_range_added_soh = 100 * (range_gained / soc_diff) / @FORD_ETRANSIT_DEFAULT_KM_PER_SOC")
         .eval("sec_per_soc = sec_duration / soc_diff")
     )