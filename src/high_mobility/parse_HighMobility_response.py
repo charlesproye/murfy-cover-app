02a17e2c00d6d326ec6d83cc55ac4620bd7291b3
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-08-01 16:33:48 +0200

    Feat: Implemented S3_bucket class to facilitate interaction with s3 bucket. Feat: Implmented ETL script to convert responses into raw parquet time series. Fix: Fixed flatten_json_obj

--- src/high_mobility/parse_HighMobility_response.py
+++ src/high_mobility/parse_HighMobility_response.py
@@ -1,7 +1,7 @@
 """
 This module implements a the `parse_json_obj` function.  
 It converts a HighMobility response dict into a df.  
-When called as a script, it takes in two mandatory path arguments: `json_source`, `csv_dest`.
+When called as a script, it takes in two mandatory path arguments: `json_source` and `csv_dest`.
 """
 from typing import Any
 import json
@@ -15,16 +15,18 @@ from core.argparse_utils import parse_kwargs
 
 def main():
     kwargs = parse_kwargs(["json_source", "csv_dest"])
-    df = parse_json_obj(kwargs["json_source"])
+    df = parse_json_as_df(kwargs["json_source"])
     df.to_csv(read_json(kwargs["csv_dest"]))
 
-def parse_json_obj(src) -> DF:
-    flatten_dict = flatten_json_obj(src)
+def parse_json_as_df(src) -> DF:
+    flatten_dict = flatten_json_obj(src, {})
     df = DF.from_dict(flatten_dict, orient="index").pipe(set_date)
 
     return df
 
 def set_date(df:DF) -> DF:
+    if df.empty:
+        return df
     date = df.index.to_series().dt.as_unit("s")
     df["date"] = date
     df.index = date
@@ -32,7 +34,7 @@ def set_date(df:DF) -> DF:
 
     return df
 
-def flatten_json_obj(src:dict, dst:dict[Any,dict[str,Any]]={}, timestamp=None, path:list[str]=[]) -> dict[Any,dict[str,Any]]:
+def flatten_json_obj(src:dict, dst, timestamp=None, path:list[str]=[]) -> dict[Any,dict[str,Any]]:
     if "timestamp" in src:
         timestamp = DT.strptime(src["timestamp"], "%Y-%m-%dT%H:%M:%S.%fZ")
 
--- src/high_mobility/response_to_raw_parquet.py
+++ src/high_mobility/response_to_raw_parquet.py
@@ -2,44 +2,37 @@
 This script converts High Mobility json responses into raw parquet time series.
 The script operates on S3 buckets. 
 """
-import os
-from dotenv import load_dotenv
-import requests
+from os.path import join
 import json
-from urllib.parse import urlencode, quote
-from datetime import datetime
-from io import BytesIO, StringIO
 
 from rich import print
-import boto3
+from rich.traceback import install as install_rich_traceback
+import pandas as pd
+from pandas import Series
+from pandas import DataFrame as DF
 
-from high_mobility.parse_HighMobility_response import parse_json_obj
+from core.caching_utils import save_cache_to, ensure_that_dirs_exist
+from high_mobility.parse_HighMobility_response import parse_json_as_df
+from core.s3_utils import S3_Bucket
 
 def main():
-    s3 = boto3.client(
-        "s3",
-        region_name="fr-par",
-        endpoint_url="https://s3.fr-par.scw.cloud",
-        aws_access_key_id=os.getenv("S3_KEY") or "",
-        aws_secret_access_key=os.getenv("S3_SECRET") or ""
-    )
-    # s3.list_objects_v2(Bucket="bib-platform-dev-data")
-    response = s3.get_object(Bucket="bib-platform-dev-data", Key="renault/response/renault-2024-07-31 15:30:45.275147.json")
-    content = json.loads(response["Body"].read().decode('utf-8'))
-    # print(content)
-    df = parse_json_obj(content)
-    dataframe_to_s3(s3, df, "bib-platform-dev-data", "renault/parquet/test.parquet")
+    install_rich_traceback(extra_lines=0, width=130)
+    bucket = S3_Bucket()
+    
+    keys = Series(bucket.list_keys("response"))
+    keys = keys[keys.str.endswith(".cbor")]
+    keys = pd.concat((keys, keys.str.split("/", expand=True).loc[:, 1:]), axis="columns")
+    keys.columns = ["key", "brand", "vin", "file"]
+    print(keys)
+    keys.groupby(["brand", "vin"]).apply(parse_responses_as_raw_ts, bucket, include_groups=False)
 
-def dataframe_to_s3(s3_client, input_datafame, bucket_name, filepath, format="parquet"):
-    if format == 'parquet':
-        out_buffer = BytesIO()
-        input_datafame.to_parquet(out_buffer, index=False)
-
-    elif format == 'csv':
-        out_buffer = StringIO()
-        input_datafame.to_parquet(out_buffer, index=False)
-
-    s3_client.put_object(Bucket=bucket_name, Key=filepath, Body=out_buffer.getvalue())
+def parse_responses_as_raw_ts(src_keys:DF, bucket:S3_Bucket):
+    raw_jsons:Series = src_keys["key"].apply(bucket.read_cbor)
+    raw_df:DF = pd.concat([parse_json_as_df(raw_json) for raw_json in raw_jsons])
+    raw_df = raw_df[~raw_df.duplicated()].sort_index()
+    dest_key = "/".join(["parquet", *src_keys.name]) + ".parquet"
+    print(dest_key)
+    bucket.save_pandas_obj_as_parquet(raw_df, dest_key)
 
 
 if __name__ == "__main__":
