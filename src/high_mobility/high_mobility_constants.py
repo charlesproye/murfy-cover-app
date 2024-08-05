f66eb4485fb6f30800272b8e302c31ff1d2b3814
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-08-05 15:54:54 +0200

    Feat: First implementation of raw ts processing.

--- src/high_mobility/high_mobility_constants.py
+++ src/high_mobility/high_mobility_constants.py
@@ -2,24 +2,30 @@ import pandas as pd
 
 from core.constants import *
 
+# Variables name standardization
 COLS_NAME_DICTS = {
     "diagnostics.odometer": "odometer",
-    "charging.battery_level": "raw_soc",
+    "charging.battery_level": "charging_soc",
+    "diagnostics.battery_level": "diagnostics_soc",
     "charging.status": "charging_status",
     "charging.estimated_range": "estimated_range",
-    "charging.max_range": "charging_max_range",
-    "charging.starter_battery_state": "charging_starter_battery_state",
-    "charging.charging_rate.kilowatts": "charging_power",
-    "charging.max_range": "charging_max_range",
-    "charging.battery_level_at_departure": "raw_soc_at_end_of_charge",
-    "charging.preconditioning_departure_status": "charging_preconditioning_departure_status",
-    "charging.preconditioning_remaining_time": "charging_preconditioning_remaining_time",
-    "charging.smart_charging_status": "smart_charging_status",
+    "charging.charging_rate": "charging_power",
 }
-
+# Unit standar dization
 UNIT_CONVERSION_OPS = {
     "kilometers": lambda col: col,
     "kilowatts": lambda col: col,
     "miles": lambda col: col * MILES_TO_KM,
     "minutes": lambda col: pd.to_timedelta(col, "minutes")
 }
+# Battery state from charging status variable
+CHARGING_STATUS_TO_CHARGING_VAL = ['charging']
+CHARGING_STATUS_TO_DISCHARGING_VAL = [
+  'cable_unplugged',
+  'charging_complete',
+  'charging_paused',
+  'not_charging',
+  'charging_error',
+  'flap_open',
+]
+#{nan: 1708, 'cable_unplugged': 278, 'charging': 34, 'charging_complete': 61, 'charging_paused': 8, 'not_charging': 80, 'charging_error': 3, 'flap_open': 1}
--- src/high_mobility/high_mobility_processed_ts.py
+++ src/high_mobility/high_mobility_processed_ts.py
@@ -6,20 +6,23 @@ Like what vehicule has power data for example.
 import logging
 
 from rich import print
-from rich.traceback import install as install_rich_traceback
 import pandas as pd
 from pandas import Series
 from pandas import DataFrame as DF
+import numpy as np
 
+from core.time_series_processing import in_discharge_and_charge_from_soc_diff
 from core.s3_utils import S3_Bucket
 from core.constants import *
+from core.console_utils import main_decorator
 from high_mobility.high_mobility_constants import *
 from core.pandas_utils import print_data
 
 logger = logging.getLogger(__name__)
 
+@main_decorator
 def main():
-    install_rich_traceback(extra_lines=0, width=130)
+    logging.basicConfig(level=logging.INFO)
     bucket = S3_Bucket()
     # Get list of objects in response key
     keys = Series(bucket.list_keys("raw_ts/time_series"))
@@ -30,10 +33,10 @@ def main():
     keys["vin"] = keys["vin"].str.strip(".parquet")
     # for each group of responses for a vin create a raw time series parquet
     # print(keys)
-    processed_ts_metada_data = keys.apply(raw_ts_ETL, bucket=bucket, axis=1)
-    bucket(processed_ts_metada_data, "processed_ts/metadata.parquet")
+    processed_ts_metada_data = keys.apply(processed_ts_ETL, bucket=bucket, axis=1)
+    # bucket.save_pandas_obj_as_parquet(processed_ts_metada_data, "processed_ts/ts_metadata.parquet")
 
-def raw_ts_ETL(key_data:Series, bucket: S3_Bucket) -> Series:
+def processed_ts_ETL(key_data:Series, bucket: S3_Bucket) -> Series:
     """
     ### Description:
     Extracts, processes and loads the raw time series.
@@ -41,18 +44,19 @@ def raw_ts_ETL(key_data:Series, bucket: S3_Bucket) -> Series:
     Metadata about the time series.
     """
     # print(key_data)
+    print(key_data[["brand", "vin"]])
     raw_ts = bucket.read_parquet(key_data["key"])
     processed_ts = process_raw_ts(raw_ts, key_data)
     print("=================")
     dest_key = "/".join(("processed_ts", "time_series", key_data["brand"], key_data["vin"])) + ".parquet"
-    # bucket.save_pandas_obj_as_parquet(processed_ts, dest_key)
+    bucket.save_pandas_obj_as_parquet(processed_ts, dest_key)
 
-    return compute_metada_data(processed_ts)
+    return compute_metada_data(processed_ts, key_data)
 
 def compute_metada_data(processd_ts: DF, key_data:Series) -> Series:
     return Series({
         "vin": key_data["vin"],
-        "has_power_during_chare": ""
+        # "has_power_during_chare": processd_ts
     })
 
 def process_raw_ts(ts:DF, key_data:Series) -> DF:
@@ -70,12 +74,18 @@ def process_raw_ts(ts:DF, key_data:Series) -> DF:
     - in_discharge  
     *Do not merge the last two into one column as we sometime don't know if the vehicule is charging or discharging.*
     """
+    print(ts)
     return (
         ts
+        # For some reason, date index seems to get reset when written or read from or to s3...
+        .set_index("date", drop=False) 
         .drop_duplicates()
-        .pipe(standardize_units, key_data)
-        .pipe(print_data)
         .rename(columns=COLS_NAME_DICTS)
+        .pipe(standardize_units, key_data)
+        .pipe(standaridize_soc, "charging_soc")
+        .pipe(standaridize_soc, "diagnostics_soc")
+        .assign(soc=lambda df:df.get("diagnostic_soc", Series([np.nan] * len(df), dtype="float")))
+        .pipe(compute_charging_status_columns)
         .pipe(print_data)
     )
 
@@ -97,10 +107,27 @@ def standardize_units(ts: DF, key_data:Series) -> DF:
 
     return ts
 
-def compute_charging_status_columns(ts: DF) -> DF:
-    # Cases:
-    if "charging_status" in ts.columns: # 
-        
+def standaridize_soc(ts:DF, soc_col:str="soc") -> DF:
+    """Ensures that the raw soc is a percentage."""
+    if soc_col in ts.columns and ts[soc_col].max() <= 1:
+        ts[soc_col] *= 100
+    return ts
 
+def compute_charging_status_columns(ts: DF) -> DF:
+    """
+    ### Description:
+    Ensure that the time series has a `in_charge` and `in_discharge` column.  
+    If the data API provides a charging status it will be used to compute the masks.  
+    Otherwise, the soc will be used.  
+    """
+    # Cases:
+    if "charging_status" in ts.columns: 
+        ts["in_charge"] = ts["charging_status"].isin(CHARGING_STATUS_TO_CHARGING_VAL)
+        ts["in_discharge"] = ts["charging_status"].isin(CHARGING_STATUS_TO_DISCHARGING_VAL)
+    else:
+        ts = in_discharge_and_charge_from_soc_diff(ts)
+    
+    return ts
+    
 if __name__ == "__main__":
     main()
