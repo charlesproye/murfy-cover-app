d50b1163ca7b9479332523bc8d5889356fd3f415
Mauro Abidal Carrer <mauroabida@yahoo.fr>
2024-08-13 16:24:24 +0200

    Feat: Implemented knn regression soh estimation. (will most likely be replaced by LR soh estimation per charging regime)

--- src/watea/energy_distribution.py
+++ src/watea/energy_distribution.py
@@ -1,94 +1,103 @@
 # Currently this is in watea but it's destined to be moved to core when we will use it for tesla
+from os.path import exists
+
 from pandas import DataFrame as DF
 from pandas import Series
 import pandas as pd
 import numpy as np
+from sklearn.neighbors import KNeighborsRegressor
 
 import core.perf_agg_processing as perfs
 from core.pandas_utils import floor_to, series_start_end_diff, log_data_and_return_same_data
+from core.caching_utils import save_cache_to
 from watea.watea_constants import *
 from watea.watea_fleet_info import fleet_info_df
+from watea.processed_watea_ts import processed_ts_it
 
-def soh_from_intercepts(vehicle_charge_dist_intercept: Series, default_100_soh_intercepts: Series) -> Series:
+# soh estimation evaluation
+
+# soh estimation
+def estimate_soh(charging_points:DF) -> DF:
     return (
-        vehicle_charge_dist_intercept
-        .div(default_100_soh_intercepts, level=1)
-        .mul(100)
-        .groupby(level=0)
-        .median()
+        charging_points
+        .eval("soh = energy_added / default_100_soh_energy_added") # All of that just for a division...
     )
 
-def dists_from_default_dist_and_intercepts(intercepts: Series, default_dist_shape: Series) -> Series:
-    """
-    ### Descriptoion:
-    Creates a synthetic energy distribution that is the sum of the default distribution shape + the intercepts.
-    """
+def estimate_default_100_soh_energy_added(charging_points:DF, feature_cols=DEFAULT_100_SOH_FEATURES, n_neighbors: int = 4, p=2) -> DF:
+    fit_samples = charging_points.query("is_default_100_soh")
+    regressor = (
+        KNeighborsRegressor(n_neighbors=n_neighbors, weights="distance", p=p)
+        .fit(fit_samples[feature_cols].values, fit_samples["energy_added"].values)
+    )
     return (
-        intercepts
-        .groupby(level=[0, 1])
-        .apply(lambda intercept: Series((default_dist_shape + intercept.iat[0]).values, index=default_dist_shape.index)) 
+        charging_points
+        .assign(default_100_soh_energy_added=regressor.predict(charging_points[feature_cols]))
     )
 
-# TODO: Try to merge the two diffs functions
-def all_diffs_from_points_to_dist(medians: Series, dist_shape) -> Series:
-    return (
-        medians
-        .groupby(level=[0, 1])
-        .apply(diff_from_points_to_dist, dist_shape)
+# Preprocessing
+def compute_regime_seperation_feature(fleet_charging_points:DF) -> DF:
+    expected_voltage_from_soc = (
+        fleet_charging_points
+        .loc[:, ["voltage", "soc"]]
+        .drop_duplicates()
+        .sort_values("soc")
+        .rolling(80, center=True, on="soc")
+        .min()
+        .rolling(80, center=True)
+        .min()
+        .dropna()
     )
-# TODO: Change the soc aggregation in compute_charge_energy_points_df to 0.5 soc and remove the roudning operation here
-# Use .sub(dist_shape).median instead of np.median(median.values - dist_shape.loc[idx]) 
-# Convert the entire function into chain calling style 
-def diff_from_points_to_dist(median: Series, dist_shape: Series) -> float:
-    median = median.drop_duplicates()
-    idx = median.index.get_level_values(2) #(median.index.get_level_values(2) * 2).round() / 2
-    
-    return np.median(median.values - dist_shape.loc[idx]) 
-
-def fit_poly_lr_to_charge_dist_xs(dist_median_xs: DF) -> Series:
-    charge_energy_distribution = (
+    expected_voltage = (
         CHARGE_ENERGY_POINTS_TO_DIST_MODEL
-        .fit(dist_median_xs.index.values, dist_median_xs.values)
-        .predict(VOLTAGE_RANGE)
+        .fit(expected_voltage_from_soc["soc"].values, expected_voltage_from_soc["voltage"].values)
+        .predict(fleet_charging_points["soc"].values)
         .squeeze()
     )
-    fitted_fleet_charge_energy_dist_xs = (
-        Series(charge_energy_distribution, VOLTAGE_RANGE)
-        .clip(300, 500)
-    )
+    fleet_charging_points["regime_seperation_feature"] = fleet_charging_points["voltage"] - expected_voltage
+    
+    return fleet_charging_points
 
-    return fitted_fleet_charge_energy_dist_xs
-
-def compute_charge_energy_median(charge_energy_points_df: DF) -> pd.Series:
+# Extraction:
+def agg_charging_points_over_charges(charging_points:DF, agg_dict:dict=CHARGING_POINTS_AGG_OVER_CHARGES_DICT) -> DF:
+    agg_dict = {key: value for key, value in agg_dict.items() if key in charging_points.columns}
     return (
-        charge_energy_points_df
-        .query(MOST_COMMON_CHARGE_REGIME_QUERY)
-        .loc[:, "energy_added"]
-        .groupby(level=[0, 1, 2])
-        .agg("median")
-        .groupby(level=[0, 1])
-        .rolling(4, center=True, min_periods=1)
-        .median()
-        .dropna()
-        .droplevel([0, 1])
+        charging_points
+        .groupby("charge_id")
+        .agg(agg_dict)
     )
 
-def aggregate_energy_points_across_charges(energy_points:DF) -> DF:
+def clean_charging_points(charging_points:DF) -> DF:
     return (
-        energy_points
-        .groupby(level=[0, 1, 3])
-        .agg("mean")
-        .drop(columns=["charge_idx", "charge_id"])
+        charging_points
+        .dropna(how="any")
+        .query("energy_added < 502 & energy_added > 100")
     )
 
-def compute_charging_points(ts:DF, id:str) -> DF:
+def get_raw_fleet_charging_points(force_update=False) -> DF:
+    if exists(PATH_TO_RAW_FLEET_CHARGING_POINTS) and not force_update:
+        return pd.read_parquet(PATH_TO_RAW_FLEET_CHARGING_POINTS)    
+    else:
+        charging_poitns = extract_raw_fleet_charging_points()
+        save_cache_to(charging_poitns, PATH_TO_RAW_FLEET_CHARGING_POINTS)
+        return charging_poitns
+        
+def extract_raw_fleet_charging_points() -> DF:
+    return (
+        pd.concat(
+            [extract_charging_points_of_ts(ts, id) for id, ts in processed_ts_it("has_power_during_charge")],
+            ignore_index=True
+        )
+        .eval("is_default_100_soh = odometer <= 3000")
+    )
+
+def extract_charging_points_of_ts(ts:DF, id:str) -> DF:
     # Use the index/location of the id of the ts in the fleet_info_df to track back the origin of energy points during debugging
     # TODO: use hashing or another method that is only dependent on the id instead of the location in the fleet info order
     id_idx = fleet_info_df.index.get_loc(id) 
     return (
         ts
         .assign(ffilled_odometer=ts["odometer"].ffill())
-        .assign(floored_soc=floor_to(ts["soc"].ffill(), ENERGY_POINTS_GRP_BY_SOC_QUANTIZATION))
+        .assign(floored_soc=floor_to(ts["soc"].ffill(), CHARGING_POINTS_GRP_BY_SOC_QUANTIZATION))
         .query("in_charge_perf_mask")
         .groupby(["in_charge_perf_idx","floored_soc",], sort=True)
         # TODO: Use pd.NamedAgg & agg kwargs instead of agg & rename
