24dc3eb129bc6c0ecc4a2e61e9dec32a142a0c12
Tom Béné <tom@bib-batteries.fr>
2024-08-29 16:41:57 +0200

    feat: made merge wrapper generic

--- src/ingestion/high_mobility/multithreading.py
+++ src/ingestion/high_mobility/multithreading.py
@@ -1,37 +1,43 @@
 from threading import Lock
-from typing import Optional
+from typing import Generic, Type, TypeVar
 
 from ingestion.high_mobility.schema import (
+    BmwInfo,
     KiaInfo,
     MercedesBenzInfo,
-    MergedKiaInfo,
-    MergedMercedesBenzInfo,
-    MergedRenaultInfo,
     RenaultInfo,
 )
+from ingestion.high_mobility.schema.merged import (
+    MergedInfoProtocol,
+)
+
+U = TypeVar("U", BmwInfo, KiaInfo, MercedesBenzInfo, RenaultInfo)
+V = TypeVar("V", bound=MergedInfoProtocol)
 
 
-class MergedInfoWrapper:
-    info: Optional[MergedKiaInfo | MergedRenaultInfo | MergedMercedesBenzInfo]
+class MergedInfoWrapper(Generic[U, V]):
+    info: V
     __lock: Lock
 
-    def __init__(self) -> None:
-        self.info = None
+    def __init__(self, typ: Type[V]) -> None:
+        self.info = typ.new()
         self.__lock = Lock()
 
-    def set_info(self, info: KiaInfo | RenaultInfo | MercedesBenzInfo) -> None:
+    def set_info(self, info: U) -> None:
         with self.__lock:
-            match info:
-                case KiaInfo():
-                    self.info = MergedKiaInfo.from_initial(info)
-                case RenaultInfo():
-                    self.info = MergedRenaultInfo.from_initial(info)
-                case MercedesBenzInfo():
-                    self.info = MergedMercedesBenzInfo.from_initial(info)
-                case _:
-                    return
+            self.info = type(self.info).from_initial(info)
+            # match info:
+            #     case KiaInfo():
+            #         self.info = MergedKiaInfo.from_initial(info)
+            #     case RenaultInfo():
+            #         self.info = MergedRenaultInfo.from_initial(info)
+            #     case MercedesBenzInfo():
+            #         self.info = MergedMercedesBenzInfo.from_initial(info)
+            #     case _:
+            #         return
 
-    def merge(self, other):
+    def merge(self, other: U):
         with self.__lock:
-            self.info.merge(other)
+            if self.info:
+                self.info.merge(other)
 
