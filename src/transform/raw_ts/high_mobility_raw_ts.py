8ab438937498627ef5e3fd08180b880cd1318f58
Martin Vaz <martin.vaz@bib-batteries.fr>
2024-09-26 11:13:57 +0200

    wip: study renault soh-battery_energy

--- src/transform/raw_ts/high_mobility_raw_ts.py
+++ src/transform/raw_ts/high_mobility_raw_ts.py
@@ -53,7 +53,6 @@ class HighMobilityRawTS(Jobinterval):
                 No raw time series have been generated.
             """)
             return
-        # print(keys)
         # Only retain .json responses
         keys = keys[keys.str.endswith(".json")]
         # Reponses are organized as follow response/brand_name/vin/date-of-response.json
@@ -68,11 +67,10 @@ class HighMobilityRawTS(Jobinterval):
         )
         keys["is_valid_file"] &= keys["vin"].str.len() != 0
         keys = keys.query(f"is_valid_file")
-        # print(keys)
         # for each group of responses for a vin create a raw time series parquet and get the meta data
         meta_data: Series =  keys.groupby("vin").apply(self.parse_responses_of_single_vin_as_raw_ts, include_groups=False)
         meta_data = DF(meta_data.to_list(), index=meta_data.index)
-        print(meta_data)
+
         
 
     def parse_responses_of_single_vin_as_raw_ts(self, src_keys:DF):
@@ -95,9 +93,6 @@ class HighMobilityRawTS(Jobinterval):
             return
         raw_ts:DF = pd.concat(parsed_raw_tss, ignore_index=True)
         dest_key = "/".join(["raw_ts", self.brand, "time_series", src_keys.name]) + ".parquet"
-        # print(dest_key)
-        # print(raw_ts)
-        # print("==============")
         self.bucket.save_df_as_parquet(raw_ts, dest_key)
 
         return raw_ts.columns
@@ -113,7 +108,6 @@ class HighMobilityRawTS(Jobinterval):
         flattened_response:dict = {}
         for capability, variables in response.items():
             if not isinstance(variables, dict):
-                # print(variables)
                 continue
             for variable, elements in variables.items():
                 for element in elements:
