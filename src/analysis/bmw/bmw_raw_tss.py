f03dab3a827bfbc2e5d5175c5f5949e1e97a3e1c
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-01 19:52:23 +0200

    Fix: Fixed missing value calculation in the notebook.

--- src/analysis/bmw/bmw_raw_tss.py
+++ src/analysis/bmw/bmw_raw_tss.py
@@ -11,11 +11,19 @@ def get_raw_tss(bucket: S3_Bucket) -> DF:
     keys = bucket.list_responses_keys_of_brand("BMW")
     return pd.concat([parse_response_as_raw_ts(key, bucket) for _, key in keys.iterrows()])
 
-def parse_response_as_raw_ts(key:str, bucket:S3_Bucket) -> DF:
-    api_response = bucket.read_json_file(key["key"])                            # The json response contains a "data" key who's values are a list of dicts.
-    raw_ts = DF.from_dict(api_response["data"])                                 # The dicts have a "key" and "value" items.
-    unit_not_none = raw_ts["unit"].notna()                                      # They also have a "unit" item but not all of them are not null.
-    raw_ts.loc[unit_not_none, "key"] += "_" + raw_ts.loc[unit_not_none, "unit"] # So we append "_" + "unit" to the key only if the "unit" is not none.
+# Small work around for the raw_ts notebook
+# TODO: Remove once we have the processed TS and use processed ts in the notebook (that will be called processed_ts_EDA I guess).
+@singleton_s3_data_caching(BMW_RAW_TSS_KEY_WITHOUT_UNITS)
+def get_raw_tss_without_units(bucket: S3_Bucket) -> DF:
+    keys = bucket.list_responses_keys_of_brand("BMW")
+    return pd.concat([parse_response_as_raw_ts(key, bucket, add_units=False) for _, key in keys.iterrows()])
+
+def parse_response_as_raw_ts(key:str, bucket:S3_Bucket, add_units=True) -> DF:
+    api_response = bucket.read_json_file(key["key"])                                # The json response contains a "data" key who's values are a list of dicts.
+    raw_ts = DF.from_dict(api_response["data"])                                     # The dicts have a "key" and "value" items.
+    if add_units:
+        unit_not_none = raw_ts["unit"].notna()                                      # They also have a "unit" item but not all of them are not null.
+        raw_ts.loc[unit_not_none, "key"] += "_" + raw_ts.loc[unit_not_none, "unit"] # So we append "_" + "unit" to the key only if the "unit" is not none.
     raw_ts = (
         raw_ts
         .pipe(
@@ -23,15 +31,16 @@ def parse_response_as_raw_ts(key:str, bucket:S3_Bucket) -> DF:
             columns="key",
             values="value",
             index="date_of_value",
-            aggfunc="first"
+            aggfunc="first",
+            dropna=False,
         )
         .assign(vin=key["vin"])
         .reset_index(drop=False)
     )
-    
+
     return raw_ts
 
-    
+
 if __name__ == "__main__":
     single_dataframe_script_main(
         get_raw_tss,
