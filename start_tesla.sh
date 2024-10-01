da75a8fd3e5712063ba5b6667c8a62d9853b5217
Théophile Marchand-Arvier <theophile@bib-batteries.fr>
2024-10-01 19:40:41 +0200

    add header

--- start_tesla.sh
+++ start_tesla.sh
@@ -5,43 +5,35 @@ set -a
 source .env
 set +a
 
-# ACCOUNTS_JSON='[
-#   {
-#     "access_token_key": "ACCESS_TOKEN_ROADSTER",
-#     "refresh_token_key": "REFRESH_TOKEN_ROADSTER",
-#     "professional_account": true
-#   },
-#   {
-#     "access_token_key": "ACCESS_TOKEN_PA",
-#     "refresh_token_key": "REFRESH_TOKEN_PA",
-#     "professional_account": false,
-#     "vehicle_id": "LRWYGCFS6PC552861"
-#   },
-#   {
-#     "access_token_key": "ACCESS_TOKEN_BEN",
-#     "refresh_token_key": "REFRESH_TOKEN_BEN",
-#     "professional_account": false,
-#     "vehicle_id": "5YJ3E7EB1KF334219"
-#   },
-#   {
-#     "access_token_key": "ACCESS_TOKEN_EGGERMONT",
-#     "refresh_token_key": "REFRESH_TOKEN_EGGERMONT",
-#     "professional_account": false,
-#     "vehicle_id": "5YJ3E7EB7KF474436"
-#   },
-#   {
-#     "access_token_key": "ACCESS_TOKEN_BERNIE",
-#     "refresh_token_key": "REFRESH_TOKEN_BERNIE",
-#     "professional_account": false,
-#     "vehicle_id": "5YJSA7H11EFP63486"
-#   }
-# ]'
-
 ACCOUNTS_JSON='[
   {
     "access_token_key": "ACCESS_TOKEN_ROADSTER",
     "refresh_token_key": "REFRESH_TOKEN_ROADSTER",
     "professional_account": true
+  },
+  {
+    "access_token_key": "ACCESS_TOKEN_PA",
+    "refresh_token_key": "REFRESH_TOKEN_PA",
+    "professional_account": false,
+    "vehicle_id": "LRWYGCFS6PC552861"
+  },
+  {
+    "access_token_key": "ACCESS_TOKEN_BEN",
+    "refresh_token_key": "REFRESH_TOKEN_BEN",
+    "professional_account": false,
+    "vehicle_id": "5YJ3E7EB1KF334219"
+  },
+  {
+    "access_token_key": "ACCESS_TOKEN_EGGERMONT",
+    "refresh_token_key": "REFRESH_TOKEN_EGGERMONT",
+    "professional_account": false,
+    "vehicle_id": "5YJ3E7EB7KF474436"
+  },
+  {
+    "access_token_key": "ACCESS_TOKEN_BERNIE",
+    "refresh_token_key": "REFRESH_TOKEN_BERNIE",
+    "professional_account": false,
+    "vehicle_id": "5YJSA7H11EFP63486"
   }
 ]'
 
