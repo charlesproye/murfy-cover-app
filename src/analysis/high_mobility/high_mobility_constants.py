7909e9b1a7ef70b9b22fbf943a186da73e4c989d
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-02 18:34:27 +0200

    Feat: Implemented proper high mobility raw ts ETL.

---
+++ src/analysis/high_mobility/high_mobility_constants.py
@@ -0,0 +1,2 @@
+HM_RAW_TSS_KEY_FORMAT = "raw_ts/{brand}/time_series/raw_tss.parquet"
+HM_RAW_TSS_KEY_FORMAT = "processed_ts/{brand}/time_series/raw_tss.parquet"
---
+++ src/analysis/high_mobility/high_mobility_raw_tss.py
@@ -0,0 +1,66 @@
+from dateutil import parser
+import logging
+
+import pandas as pd
+from pandas import DataFrame as DF
+from pandas import Series
+from rich.progress import track
+
+from core.s3_utils import S3_Bucket
+from core.console_utils import single_dataframe_script_main
+from core.caching_utils import instance_s3_data_caching
+from core.pandas_utils import concat
+from analysis.high_mobility.high_mobility_constants import *
+
+@instance_s3_data_caching(HM_RAW_TSS_KEY_FORMAT, ["brand"])
+def get_raw_tss(brand:str, bucket: S3_Bucket) -> DF:
+    return (
+        bucket.list_responses_keys_of_brand(brand)
+        .apply(parse_response_as_raw_ts, axis="columns", bucket=bucket)
+        .pipe(concat)
+    )
+
+def parse_response_as_raw_ts(key:Series, bucket:S3_Bucket, logger=logging.getLogger("HM_raw_tss")) -> DF:
+    # The responses are first indexed by "capability" (see any high mobility's air table data catalog).
+    # We don't really need to know what capability but some variables that belong to different capabilities may have the same name.
+    # To differentiate them, we will prepend their correspomding capability to their name.
+    print(key)
+    response = bucket.read_json_file(key["key"])
+    if response is None:
+        logger.info(f"Did not parse key {key['key']} because the object returned by read_json_file was None.")
+        return Series([])
+    flattened_response:dict = {}
+    for capability, variables in response.items():
+        if not isinstance(variables, dict):
+            continue
+        for variable, elements in variables.items():
+            for element in elements:
+                timestamp = parser.isoparse(element["timestamp"])
+                variable_name = capability + "." + variable 
+                if isinstance(element["data"], dict):
+                    if not "value" in element["data"]:
+                        continue
+                    value = element["data"]["value"]
+                    if "unit" in element:
+                        variable_name += "." + element["unit"]
+                else:
+                    value = element["data"]
+                flattened_response[timestamp] = flattened_response.get(timestamp, {}) | {variable_name: value}
+    raw_ts = (
+        DF.from_records(flattened_response)
+        .T
+        .reset_index(drop=False, names="date")
+        .assign(vin=key["vin"])
+    )
+
+    return raw_ts
+
+
+if __name__ == "__main__":
+    single_dataframe_script_main(
+        get_raw_tss,
+        bucket=S3_Bucket(),
+        force_update=True,
+        brand="kia",
+    )
+
