ceb83202454212d55337e58be73525234741f4fe
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-04 14:49:18 +0200

    Refactor: Refactored get_ayvens_raw_tss to get dict of dataframes instead of monolytic dataframe.

--- src/transform/ayvens/ayvens_get_raw_tss.py
+++ src/transform/ayvens/ayvens_get_raw_tss.py
@@ -13,8 +13,7 @@ from core.pandas_utils import concat, uniques_as_series
 from transform.ayvens.ayvens_fleet_info import fleet_info
 from transform.config import *
 
-@instance_s3_data_caching(S3_RAW_TSS_KEY_FORMAT.format(brand="ayvens"))
-def get_ayvens_raw_tss(bucket: S3_Bucket=S3_Bucket()) -> DF:
+def get_ayvens_raw_tss(bucket: S3_Bucket=S3_Bucket()) -> dict[str, DF]:
     logger = getLogger(f"transform.Ayvens-RawTSS")
     makers = (
         fleet_info
@@ -22,18 +21,18 @@ def get_ayvens_raw_tss(bucket: S3_Bucket=S3_Bucket()) -> DF:
         .loc[:, "make"]
         .pipe(uniques_as_series)
     )
-    print(makers)
-    return (
-        makers
-        .apply(get_raw_tss_of_brand, bucket=bucket)
-        .pipe(concat)
-    )
+    raw_tss = makers.apply(get_raw_tss_of_brand, bucket=bucket)
+    return dict(zip(makers, raw_tss))
+
 
 def get_raw_tss_of_brand(brand:str, bucket: S3_Bucket)  -> DF:
-    print(brand)
-    res = BRAND_PIPELINES.loc[brand, "raw_tss"](brand=brand, bucket=bucket)
-    print(res)
-    return res
+    brand_raw_tss = BRAND_PIPELINES.loc[brand, "raw_tss"](brand=brand, bucket=bucket)
+    ayvens_vins_mask = brand_raw_tss["vin"].isin(fleet_info["vin"])
+    brand_raw_tss = brand_raw_tss[ayvens_vins_mask]
+
+    return brand_raw_tss
 
 if __name__ == "__main__":
-    single_dataframe_script_main(get_ayvens_raw_tss, force_update=True)
+    yes = get_ayvens_raw_tss()
+    print(yes)
+
