473b2dc45e51514be8858be503b576530128951e
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-07 18:05:37 +0200

    Clean: Removed useless prints.

--- src/transform/ayvens/ayvens_get_raw_tss.py
+++ src/transform/ayvens/ayvens_get_raw_tss.py
@@ -15,19 +15,18 @@ from transform.config import *
 
 def get_ayvens_raw_tss(bucket: S3_Bucket=S3_Bucket()) -> dict[str, DF]:
     logger = getLogger(f"transform.Ayvens-RawTSS")
-    print(fleet_info["activated"].unique())
     makers = (
         fleet_info
         .query("activated == 'activated'")
         .loc[:, "make"]
         .pipe(uniques_as_series)
     )
-    print(makers)
     raw_tss = makers.apply(get_raw_tss_of_brand, bucket=bucket)
     return dict(zip(makers, raw_tss))
 
 
 def get_raw_tss_of_brand(brand:str, bucket: S3_Bucket)  -> DF:
+    print(brand)
     brand_raw_tss = BRAND_PIPELINES.loc[brand, "raw_tss"](brand=brand, bucket=bucket)
     ayvens_vins_mask = brand_raw_tss["vin"].isin(fleet_info["vin"])
     brand_raw_tss = brand_raw_tss[ayvens_vins_mask]
@@ -35,6 +34,5 @@ def get_raw_tss_of_brand(brand:str, bucket: S3_Bucket)  -> DF:
     return brand_raw_tss
 
 if __name__ == "__main__":
-    yes = get_ayvens_raw_tss()
-    print(yes)
+    print(get_ayvens_raw_tss())
 
