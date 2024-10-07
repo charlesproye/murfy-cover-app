1c0b649e93ed7760a2e2c52388a83723bff006dd
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-07 15:40:29 +0200

    Fix:Forgot to add contract dates from global NL.

--- src/transform/ayvens/ayvens_fleet_info.py
+++ src/transform/ayvens/ayvens_fleet_info.py
@@ -26,19 +26,21 @@ def get_fleet_info() -> DF:
     fleet_info["dummy_soh_model_slope"] = fleet_info.groupby(["make", "version"])["vin"].transform(lambda vins: random.uniform(SOH_LOST_PER_KM_DUMMY_RATIO - 0.00001, SOH_LOST_PER_KM_DUMMY_RATIO + 0.00001))
     fleet_info["dummy_soh_vehicle_offset"] = fleet_info.groupby(["make", "version"])["vin"].transform(lambda vins: random.uniform(SOH_LOST_PER_KM_DUMMY_RATIO - 0.00001, SOH_LOST_PER_KM_DUMMY_RATIO + 0.00001))
 
-    # Add contract start dates from fleet info global NL (NetherLands)
-    fleet_info_with_start_conctract = pd.read_csv(AYVENS_FLEET_WITH_ONLY_CONTRACT_START_DATE).set_index("VIN", drop=False)
-    fleet_info["in_GLOBAL"] = fleet_info["vin"].isin(fleet_info_with_start_conctract["VIN"])
-    vins_in_global = fleet_info.query("in_GLOBAL")["vin"]
-    contract_start_dates = fleet_info_with_start_conctract.loc[vins_in_global, "Contract start datum"]
-    fleet_info.loc[vins_in_global, "contract_start_date"] = contract_start_dates
-
     # Add registration dates from fleet info global NL (NetherLands) 2
-    fleet_info_with_start_conctract = pd.read_csv(AYVENS_FLEET_WITH_CAR_REGISTRATION).set_index("VIN", drop=False)
-    fleet_info["in_GLOBAL2"] = fleet_info["vin"].isin(fleet_info_with_start_conctract["VIN"])
-    vins_in_global = fleet_info.query("in_GLOBAL2")["vin"]
-    car_registrations = fleet_info_with_start_conctract.loc[vins_in_global, "Car registration date"]
-    fleet_info.loc[vins_in_global, "registration date"] = car_registrations
+    fleet_info_with_registration_and_start_contract = pd.read_csv(AYVENS_FLEET_WITH_CAR_REGISTRATION).set_index("VIN", drop=False)
+    fleet_info["in_GLOBAL2"] = fleet_info["vin"].isin(fleet_info_with_registration_and_start_contract["VIN"])
+    vins_in_global2 = fleet_info.query("in_GLOBAL2")["vin"]
+    car_registrations = fleet_info_with_registration_and_start_contract.loc[vins_in_global2, "Car registration date"]
+    fleet_info.loc[vins_in_global2, "registration date"] = car_registrations
+
+    # Add contract start dates from fleet info global NL (NetherLands)
+    fleet_info_with_start_contract = pd.read_csv(AYVENS_FLEET_WITH_ONLY_CONTRACT_START_DATE).set_index("VIN", drop=False)
+    fleet_info["in_GLOBAL"] = fleet_info["vin"].isin(fleet_info_with_start_contract["VIN"])
+    vins_in_global = fleet_info.query("in_GLOBAL")["vin"]
+    contract_start_dates = fleet_info_with_start_contract.loc[vins_in_global, "Contract start datum"]
+    fleet_info.loc[vins_in_global, "contract_start_date"] = contract_start_dates
+    contract_start_dates = fleet_info_with_registration_and_start_contract.loc[vins_in_global2, "Contract start date"]
+    fleet_info.loc[vins_in_global2, "contract_start_date"] = contract_start_dates
 
     return fleet_info
 
