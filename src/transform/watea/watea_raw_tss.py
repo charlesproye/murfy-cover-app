d034fa8139be47aa7db3efd1d3cf4dbc953e541e
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-19 20:33:52 +0200

    Feat: moved the astype, drop and rename calls to per vehicle extraction.

--- src/transform/watea/watea_raw_tss.py
+++ src/transform/watea/watea_raw_tss.py
@@ -9,7 +9,7 @@ from core.caching_utils import cache_result
 
 @cache_result(RAW_TS_PATH, on="local_storage")
 def get_raw_tss() -> DF:
-    raw_tss = {}
+    raw_tss: dict[str, DF] = {}
     for path in glob.iglob(WATEA_RESPONSES_REGEX, recursive=True):
         split_path = path.split("/")
         id = split_path[-2][-6:]
@@ -17,18 +17,14 @@ def get_raw_tss() -> DF:
             pd.read_parquet(path)
             .assign(id=id)
             .astype({"id":"string"})
+            .drop(columns=COLS_TO_DROP)
+            .astype(DTYPES)
+            .rename(columns=RENAMING_MAP)
         )
-
-    raw_tss = (
-        pd.concat(raw_tss, ignore_index=True)
-        .drop(columns=COLS_TO_DROP)
-        .astype(DTYPES)
-        .rename(columns=RENAMING_MAP)
-
-    )
+    
+    raw_tss = pd.concat(raw_tss, ignore_index=True)
     
     return raw_tss
 
 if __name__ == '__main__':
     raw_tss = single_dataframe_script_main(get_raw_tss, force_update=True)
-    print(raw_tss["id"].value_counts())
