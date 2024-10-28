c9026bcf9d0310d57bb866a8df421c1e8f9fe716
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-28 11:56:45 +0100

    Fix: Fixed parameter checking in transform.raw_tss.raw_tss.get_raw_tss. Feat: Updated to use logging utils module.

--- src/transform/raw_tss/raw_tss.py
+++ src/transform/raw_tss/raw_tss.py
@@ -1,13 +1,13 @@
 from typing import Callable
-import logging.config
 import inspect
 
 from pandas import DataFrame as DF
 
-from core.console_utils import single_dataframe_script_main
 from core.s3_utils import S3_Bucket
+from core.console_utils import single_dataframe_script_main
+from core.logging_utils import set_level_of_loggers_with_prefix
 from transform.raw_tss.high_mobility_raw_tss import get_raw_tss as hm_get_raw_tss
-from transform.raw_tss.bmw_raw_tss import get_raw_tss as bmw_get_raw_tss
+from transform.raw_tss.bmw_raw_tss import get_direct_bmw_raw_tss as bmw_get_raw_tss
 from transform.raw_tss.stellantis_raw_tss import get_raw_tss as stellantis_get_raw_tss
 from transform.raw_tss.tesla_raw_tss import get_raw_tss as tesla_get_raw_tss
 
@@ -28,36 +28,14 @@ GET_RAW_TSS_FUNCTIONS:dict[str, Callable[[bool, S3_Bucket], DF]] = {
 
 def get_raw_tss(brand:str, **kwargs) -> DF:
     func = GET_RAW_TSS_FUNCTIONS[brand]
-    if "brand" in inspect.signature(func):
+    if "brand" in inspect.signature(func).parameters:
         kwargs["brand"] = brand
     
     return func(**kwargs)
 
 if __name__ == "__main__":
-    logging.config.dictConfig({
-        'version': 1,
-        "loggers": {
-            "transform": {
-                "level": logging.DEBUG,
-                'handlers': ['console'],
-                'propagate': False,
-            },
-        },
-        'handlers': {
-            'console': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'default',
-            },
-        },
-        'formatters': {
-            'default': {
-                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            },
-        },
-    })
-
+    set_level_of_loggers_with_prefix("DEBUG", "transform")
     for brand in list(GET_RAW_TSS_FUNCTIONS.keys()):
-        func = GET_RAW_TSS_FUNCTIONS[brand]
-        print(f"brand: {type(func)}")
-        single_dataframe_script_main(func, force_update=True)
+        print(brand, ":")
+        single_dataframe_script_main(get_raw_tss, brand=brand, force_update=True)
         print("============================")
