bc3f2b5152d26dacb1fbaf822e57f3f39d593fe2
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-28 16:28:19 +0100

    Feat: Added fleet_info retreaval tiem measurament job.

--- src/transform/main.py
+++ src/transform/main.py
@@ -2,14 +2,22 @@ import logging
 from datetime import datetime as DT
 from datetime import timedelta as TD
 import logging.config
+import time
 
+from pandas import Index
+from pandas import Series
+from pandas import DataFrame as DF
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
 from transform.config import *
+from core.singleton_s3_bucket import bucket
+from transform.fleet_info.ayvens_fleet_info import get_fleet_info
 from transform.raw_tss.raw_tss import update_all_raw_tss
 from core.console_utils import main_decorator, parse_kwargs
 from core.logging_utils import set_level_of_loggers_with_prefix
+
+from core.console_utils import single_dataframe_script_main
 
 @main_decorator
 def main():
@@ -31,6 +39,15 @@ def main():
             start_date=DT.now() + TD(seconds=3)
         ),
     )
+    scheduler.add_job(
+        func=record_time_to_get_ayvens_fleet_info,
+        name="data_ev_pipeline",
+        id="data_ev_pipeline",
+        trigger=IntervalTrigger(
+            hours=1,
+            start_date=DT.now() + TD(seconds=3)
+        ),
+    )
     # Start
     scheduler.start()
 
@@ -38,10 +55,28 @@ def setup_logging(transform_loggers_level=logging.INFO):
     set_level_of_loggers_with_prefix(transform_loggers_level, "transform")
     logger = logging.getLogger("apscheduler.scheduler")
     logger.setLevel(logging.INFO)
-    
+
+def record_time_to_get_ayvens_fleet_info():
+    PATH_TO_PARQUET_FLEET_INFO = "fleet_info/test_ayvens_fleet_info.parquet"
+    PATH_TO_DURATION_DF = "fleet_info/durations.parquet"
+    fleet_info = get_fleet_info()
+    bucket.save_df_as_parquet(fleet_info, PATH_TO_PARQUET_FLEET_INFO)
+    before = time.time()
+    fleet_info = bucket.read_parquet_df(PATH_TO_PARQUET_FLEET_INFO)
+    after = time.time()
+    duration = after - before
+    DEFAULT_DURATION_DF = DF(columns=["duration", "date"])
+    durations_df = DEFAULT_DURATION_DF if not bucket.check_file_exists(PATH_TO_DURATION_DF) else bucket.read_parquet_df(PATH_TO_DURATION_DF)
+    durations_df.loc[len(durations_df)] = Series({"duration": duration, "date": DT.now()})
+    bucket.save_df_as_parquet(durations_df, PATH_TO_DURATION_DF)
+
+    return durations_df
+
+
 def run_entire_pipeline():
     update_all_raw_tss()
 
 if __name__ == '__main__':
-    main()
+    # main()
+    single_dataframe_script_main(record_time_to_get_ayvens_fleet_info)
 
