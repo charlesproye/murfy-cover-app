56ea1381c6c8ec36cf3d7fc097cf2134b965fea5
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-28 12:21:06 +0100

    Feat: Added high_mobility_raw_tss.get_raw_tss in bmw_raw_tss.get_raw_tss as we are pulling bmw vehicle data from both bmw and high_mobillity.

--- src/transform/raw_tss/bmw_raw_tss.py
+++ src/transform/raw_tss/bmw_raw_tss.py
@@ -5,37 +5,37 @@ from logging import Logger, getLogger
 
 from core.s3_utils import S3_Bucket
 from core.config import S3_RAW_TSS_KEY_FORMAT
+from core.logging_utils import set_level_of_loggers_with_prefix
 from core.console_utils import single_dataframe_script_main
 from core.caching_utils import cache_result
 from core.pandas_utils import concat
 from transform.raw_tss.high_mobility_raw_tss import get_raw_tss as hm_get_raw_tss
 
-
-
 @cache_result(S3_RAW_TSS_KEY_FORMAT.format(brand="BMW"), on="s3")
-def get_raw_tss(bucket: S3_Bucket, **kwargs) -> DF:
+def get_raw_tss(bucket: S3_Bucket) -> DF:
+    return pd.concat((
+        hm_get_raw_tss("bmw", bucket=bucket, force_update=True).assign(data_provider="high_mobility"),
+        get_direct_bmw_raw_tss(bucket, append_units_to_col_names=False).assign(data_provider="bmw")
+    ))
+
+def get_direct_bmw_raw_tss(bucket: S3_Bucket, append_units_to_col_names=False) -> DF:
     logger = getLogger("transform.BMW-RawTSS")
     return (
         bucket.list_responses_keys_of_brand("BMW")
-        .apply(parse_response_as_raw_ts, axis="columns", bucket=bucket, logger=logger)
+        .apply(
+            parse_response_as_raw_ts,
+            axis="columns",
+            bucket=bucket,
+            logger=logger,
+            append_units_to_col_names=append_units_to_col_names
+        )
         .pipe(concat)
     )
 
-# Small work around for the raw_ts notebook
-# TODO: Remove once we have the processed TS and use processed ts in the notebook (that will be called processed_ts_EDA I guess).
-@cache_result(S3_RAW_TSS_KEY_FORMAT.format(brand="BMW"), on="s3")
-def get_raw_tss_without_units(bucket: S3_Bucket) -> DF:
-    logger = getLogger("transform.BMW-RawTSS")
-    return (
-        bucket.list_responses_keys_of_brand("BMW")
-        .apply(parse_response_as_raw_ts, axis="columns", bucket=bucket, logger=logger, add_units=False)
-        .pipe(concat)
-    )
-
-def parse_response_as_raw_ts(key:Series, bucket:S3_Bucket, logger:Logger, add_units=True) -> DF:
+def parse_response_as_raw_ts(key:Series, bucket:S3_Bucket, logger:Logger, append_units_to_col_names=True) -> DF:
     api_response = bucket.read_json_file(key["key"])                                # The json response contains a "data" key who's values are a list of dicts.
     raw_ts = DF.from_dict(api_response["data"])                                     # The dicts have a "key" and "value" items.
-    if add_units:
+    if append_units_to_col_names:
         unit_not_none = raw_ts["unit"].notna()                                      # They also have a "unit" item but not all of them are not null.
         raw_ts.loc[unit_not_none, "key"] += "_" + raw_ts.loc[unit_not_none, "unit"] # So we append "_" + "unit" to the key only if the "unit" is not none.
     raw_ts = (
@@ -58,6 +58,7 @@ def parse_response_as_raw_ts(key:Series, bucket:S3_Bucket, logger:Logger, add_un
 
 
 if __name__ == "__main__":
+    set_level_of_loggers_with_prefix("DEBUG", "transform")
     single_dataframe_script_main(
         get_raw_tss,
         bucket=S3_Bucket(),
