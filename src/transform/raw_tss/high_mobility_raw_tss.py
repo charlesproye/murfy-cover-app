b0152e00b024e3586e80ede2f2321e26ea8f1639
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-28 11:58:32 +0100

    Feat: Added debug calls to high_mobility_raw_tss and used new logging utils module.

--- src/transform/raw_tss/high_mobility_raw_tss.py
+++ src/transform/raw_tss/high_mobility_raw_tss.py
@@ -6,6 +6,7 @@ from pandas import DataFrame as DF
 from pandas import Series
 
 from core.config import *
+from core.logging_utils import set_level_of_loggers_with_prefix
 from core.s3_utils import S3_Bucket
 from core.console_utils import single_dataframe_script_main
 from core.caching_utils import cache_result
@@ -14,10 +15,12 @@ from core.pandas_utils import concat
 @cache_result(S3_RAW_TSS_KEY_FORMAT, path_params=["brand"], on="s3")
 def get_raw_tss(brand:str, bucket: S3_Bucket=S3_Bucket()) -> DF:
     logger = getLogger(f"transform.HighMobility-{brand}-RawTSS")
+    logger.debug(f"Starting to compute raw time series for {brand} vehicles.")
     keys = bucket.list_responses_keys_of_brand(brand)
     if keys.empty:
         logger.warning(f"No keys found for brand '{brand}'.\n Returning empty dataframe.")
         return DF()
+
     return (
         keys
         .apply(parse_response_as_raw_ts, axis="columns", bucket=bucket, logger=logger)
@@ -62,10 +65,10 @@ def parse_response_as_raw_ts(key:Series, bucket:S3_Bucket, logger:Logger) -> DF:
 
 
 if __name__ == "__main__":
+    set_level_of_loggers_with_prefix("DEBUG", "transform")
     single_dataframe_script_main(
         get_raw_tss,
         bucket=S3_Bucket(),
         force_update=True,
         brand="ford",
     )
-
