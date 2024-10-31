793b938a8e67eae746c740a3643713b1a27302b3
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-10-31 11:59:44 +0100

    Feat: Added safe_astype and merge_with_columns pandas_utils functions.

--- src/core/pandas_utils.py
+++ src/core/pandas_utils.py
@@ -15,8 +15,9 @@ def flatten_multi_indexed_columns(self: DF) -> DF:
 pd.DataFrame.flatten_multi_indexed_columns = flatten_multi_indexed_columns
 
 PD_OBJ = TypeVar('T', pd.DataFrame, pd.Series)
-def log_data_and_return_same_data(data: PD_OBJ) -> PD_OBJ:
-    logger.info(data)
+def print_data(data: PD_OBJ) -> PD_OBJ:
+    print(data)
+    print(data.dtypes)
     return data
 
 def total_MB_memory_usage(df: DF) -> int:
@@ -75,3 +76,34 @@ def set_str_to_lower(df: DF) -> DF:
     df.loc[:, str_cols] = df.loc[:, str_cols].apply(lambda col: col.str.lower())
 
     return df
+
+def merge_with_columns(df_a:DF, df_b:DF, cols_to_copy:list, merge_on:str, **merge_kwargs) -> DF:
+    """
+    Merge `df_a` with a subset of `df_b` specified by `cols_to_copy` and `merge_on`.
+
+    Parameters:
+        df_a (pd.DataFrame): The left DataFrame to merge.
+        df_b (pd.DataFrame): The right DataFrame to merge.
+        cols_to_copy (list): Columns to copy from `df_b`.
+        merge_on (list): Columns to merge on.
+        **merge_kwargs: Additional keyword arguments for pd.merge.
+    
+    Returns:
+        pd.DataFrame: The merged DataFrame.
+    """
+    # Ensure merge_on columns are included in df_b
+    selected_columns = list(set(cols_to_copy + [merge_on]))
+
+    if (merge_on in df_b.index.names) and (merge_on in df_b.columns):
+        df_b = df_b.reset_index(drop=True)
+    return df_a.merge(df_b[selected_columns], on=merge_on, how="left")
+
+
+def safe_astype(df:DF, col_dtypes:dict) -> DF:
+    """
+    Warp around pd.astype to ignore errors.
+    Removes keys from col_dtypes that are not in df.columns.
+    """
+    col_dtypes = {col:dtype for col, dtype in col_dtypes.items() if col in df.columns}
+    return df.astype(col_dtypes, errors="ignore")
+
