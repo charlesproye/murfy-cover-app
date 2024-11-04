4a0052d13bb177a5a6de640dc052130d7370611a
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-11-04 17:21:31 +0100

    Feat: Updated core.logging_utils rich logging handler.

--- src/core/logging_utils.py
+++ src/core/logging_utils.py
@@ -1,26 +1,32 @@
+from rich.logging import RichHandler
 import logging.config
 
+RICH_HANDLER = RichHandler()
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="WARNING", format=FORMAT, datefmt="[%X]", handlers=[RICH_HANDLER]
+)
 
 def set_level_of_loggers_with_prefix(level, logger_name_prefix:str):
     logging.config.dictConfig({
         'version': 1,
         "loggers": {
-            logger_name_prefix: {
-                "level": level,
-                'handlers': ['console'],
-                'propagate': False,
-            },
+            logger_name_prefix: dict(
+                level=level,
+                datefmt="[%X]",
+                handlers=["rich"],
+                propagate=False,
+            ),
         },
         'handlers': {
-            'console': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'default',
+            'rich': {
+                'class': 'rich.logging.RichHandler',
+                'formatter': 'rich',
             },
         },
         'formatters': {
-            'default': {
-                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            'rich': {
+                'format': FORMAT,
             },
         },
     })
-
