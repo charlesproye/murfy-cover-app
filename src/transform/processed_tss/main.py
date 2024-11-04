7c9390ac08eb43118dcf6c2507d3435b1c36f12f
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-11-04 17:25:07 +0100

    Feat: Updated transfrorm.processed_tss.main to correctly handle logging.

--- src/transform/processed_tss/main.py
+++ src/transform/processed_tss/main.py
@@ -2,6 +2,7 @@ from typing import Callable
 import inspect
 from logging import getLogger
 
+from rich.progress import track
 from pandas import DataFrame as DF
 
 from core.logging_utils import set_level_of_loggers_with_prefix
@@ -26,10 +27,10 @@ def get_processed_tss(brand:str, **kwargs) -> DF:
     return func(**kwargs)
 
 def update_all_processed_tss():
-    #set_level_of_loggers_with_prefix("DEBUG", "transform.processed_tss")
-    for brand in list(GET_PROCESSED_TSS_FUNCTIONS.keys()):
+    set_level_of_loggers_with_prefix("DEBUG", "transform.processed_tss")
+    for brand in track(list(GET_PROCESSED_TSS_FUNCTIONS.keys()), description="Updating processed TSSs..."):
         logger.debug(f"================={brand}=================")
-        single_dataframe_script_main(get_processed_tss, brand=brand, force_update=True)
+        single_dataframe_script_main(get_processed_tss, logger=logger, brand=brand, force_update=True)
 
 if __name__ == "__main__":
     update_all_processed_tss()
