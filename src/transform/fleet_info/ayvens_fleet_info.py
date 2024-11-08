d99ebe96abb04eb901fce3b4ed624ac60e14d100
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-11-08 18:10:23 +0100

    Feat: Used core.pandas_utils.left_merge in transform.fleet_info.ayvens_fleet_info.get_fleet_info instead of pandas' merge method.

--- src/transform/fleet_info/ayvens_fleet_info.py
+++ src/transform/fleet_info/ayvens_fleet_info.py
@@ -1,6 +1,7 @@
 from core.pandas_utils import *
 from core.config import *
 from core.s3_utils import S3_Bucket
+from core.singleton_s3_bucket import bucket
 from core.console_utils import single_dataframe_script_main
 from transform.fleet_info.config import *
 
@@ -18,12 +19,8 @@ def get_fleet_info(bucket=S3_Bucket()) -> DF:
         .drop(columns=["autonomie", "capacity"])
         .pipe(set_all_str_cols_to_lower)
         .assign(vin=lambda df: df["vin"].str.upper())
-        .merge(fleet_info_with_only_start_date, left_on="vin", right_on="VIN", how="left")
-        .rename(columns={"Car registration date": "registration_date", "Contract start date": "contract_start_date"})
-        .merge(fleet_info_with_contract_start_date, left_on="vin", right_on="VIN", how="left")
-        .rename(columns={"Contract start datum": "contract_start_date"})
-        .assign(owner="ayvens_fleet_1")
-        .set_index("vin", drop=False)
+        .pipe(left_merge, fleet_info_with_only_start_date, left_on="vin", right_on="VIN", src_dest_cols={"Car registration date": "registration_date", "Contract start date": "contract_start_date"})
+        .pipe(left_merge, fleet_info_with_contract_start_date, left_on="vin", right_on="VIN", src_dest_cols={"Contract start datum": "contract_start_date"})
     )
 
 if __name__ == "__main__":
--- src/transform/fleet_info/main.py
+++ src/transform/fleet_info/main.py
@@ -5,6 +5,7 @@ from core.console_utils import single_dataframe_script_main
 from transform.fleet_info.ayvens_fleet_info import fleet_info as ayvens_fleet_info
 from transform.fleet_info.tesla_fleet_info import fleet_info as tesla_fleet_info
 
+
 def get_fleet_info() -> DF:
     return (
         pd.concat((ayvens_fleet_info, tesla_fleet_info))
@@ -16,10 +17,8 @@ def update_fleet_info() -> DF:
     fleet_info.to_sql("vehicle", connection, if_exists="append")
     return fleet_info
 
-
 if __name__ == "__main__":
     #single_dataframe_script_main(update_fleet_info)
     single_dataframe_script_main(get_fleet_info)
 
 fleet_info = get_fleet_info()
-
