014eacee00b1e380632a2bf360cc8593b1c1b57d
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-11-10 16:31:02 +0100

    Doc: Fixed some typos in comments.

--- src/transform/fleet_info/tesla_fleet_info.py
+++ src/transform/fleet_info/tesla_fleet_info.py
@@ -9,13 +9,14 @@ def get_fleet_info(bucket: S3_Bucket=bucket) -> DF:
     return (                                                            # The response is a list of dicts.
         pd.json_normalize(response, record_path=["codes"], meta=["vin"])# The response is a list of dicts is unstructured json, so we need to normalize it. 
         # After normalizing, we end up with a DF with columns vin, code, displayName, isActive.
-        # We pivor the table and end up with a one hot encoded DF.
+        # We pivot the table and end up with a one hot encoded DF of the vehicle's features where each line is a vehicle and each column is a feature.
         .pivot_table(index="vin", columns="displayName", values="isActive", aggfunc=pd.Series.mode, fill_value=False)
         .filter(like="Model", axis=1)                                   # The only columns we are interested in are the model column so we filter out the rest.
         .idxmax(axis=1)                                                 # We take the idxmax of the row, ie:the name of the column, ie: the name of the model, to get the model of the vehicle.                
         .astype("string")                                               # We convert the dtype to string.
         .str                                                            # We use the str accessor to extract the model and version from the string.
-        .extract(r'^(?P<model>Model \w+) (?P<version>.+)$')             # We extract the model and version from the string.
+        .extract(r'^(?P<model>Model \w+) (?P<version>.+)$')             # We extract the model and version from the string with regex.
+        .reset_index(drop=False)
     )
 
 if __name__ == "__main__":
