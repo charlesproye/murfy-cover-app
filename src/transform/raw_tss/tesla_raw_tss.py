64c6ddd25d0a36ff57da0471dbd6f72b6f648084
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-11-12 19:13:41 +0100

    Fix: Fixed time series ETL imports.

--- src/transform/raw_tss/tesla_raw_tss.py
+++ src/transform/raw_tss/tesla_raw_tss.py
@@ -5,11 +5,12 @@ from pandas import DataFrame as DF
 from pandas import Series
 
 from core.config import *
+from core.logging_utils import set_level_of_loggers_with_prefix
 from core.s3_utils import S3_Bucket
 from core.console_utils import single_dataframe_script_main
 from core.caching_utils import cache_result
 from core.pandas_utils import concat
-from transform.tesla.tesla_config import *
+from transform.raw_tss.config import *
 
 
 @cache_result(S3_RAW_TSS_KEY_FORMAT.format(brand="tesla"), on="s3")
@@ -26,7 +27,12 @@ def parse_response_as_raw_ts(key: Series, bucket:S3_Bucket, logger:Logger) -> DF
     if response is None:
         logger.debug(f"Did not parse key {key['key']} because the object returned by read_json_file was None.")
         return Series([])
-    return DF.from_records(response)
+    raw_ts = DF.from_records(response)
+    raw_ts["vin"] = key["vin"]
+    logger.debug(f"Parsed {key['key']} with raw_tss tesla parser.")
+
+    return raw_ts
 
 if __name__ == "__main__":
+    set_level_of_loggers_with_prefix("DEBUG", "transform")
     single_dataframe_script_main(get_raw_tss, force_update=True)
