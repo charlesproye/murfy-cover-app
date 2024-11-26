331aa045b710041b48221d523e47860b7ecc23b9
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-11-26 10:43:59 +0100

    Feat/Clean: Set logging level of core.sql_utils in transform.fleet_info.main(Feat). Removed useless comment in transform.fleet_info.main.

--- src/transform/fleet_info/main.py
+++ src/transform/fleet_info/main.py
@@ -49,8 +49,8 @@ def get_fleet_info() -> DF:
 
 
 if __name__ == "__main__":
+    set_level_of_loggers_with_prefix("DEBUG", "core.sql_utils")
     set_level_of_loggers_with_prefix("DEBUG", "transform.fleet_info")
-    #single_dataframe_script_main(get_fleet_info)
     single_dataframe_script_main(update_db_vehicle_table) #.to_csv("fleet_info_update.csv")
 
 fleet_info = get_fleet_info()
