9431231f6dfe617902e62cafe483b5b61be52ef9
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-12-04 10:31:13 +0100

    Fix: Fixed core.consolle_utils.tss_script_main.

--- src/core/console_utils.py
+++ src/core/console_utils.py
@@ -54,14 +54,17 @@ def tss_script_main(dataframe_gen: Callable[[bool], DF], logger:Union[Logger, No
     df:DF = dataframe_gen(**kwargs)
     show = logger.info if logger is not None else print
     show(df)
-    with pd.option_context("display.max_columns", None, "display.expand_frame_repr", False):
-        show("sanity check:")
-        show(sanity_check(df))
-        print("Frequency sanity check:")
-        show(tss_frequency_sanity_check(df))
-        show(f"total memory usage: {total_MB_memory_usage(df):.2f}MB.")
+#    with pd.option_context("display.max_rows", None, "display.expand_frame_repr", False):
+    show("sanity check:")
+    show(sanity_check(df)) #.to_string(max_rows=None))
+    print("Frequency sanity check:")
+    freq_sanity_check = tss_frequency_sanity_check(df)
+    show(freq_sanity_check.to_string(max_rows=None))
+    show(freq_sanity_check.describe().to_string(max_rows=None))
+    show(f"total memory usage: {total_MB_memory_usage(df):.2f}MB.")
 
     return df
+
 @main_decorator
 def single_dataframe_script_main(dataframe_gen: Callable[[bool], DF], logger:Union[Logger, None]=None, **kwargs) -> DF:
     df:DF = dataframe_gen(**kwargs)
