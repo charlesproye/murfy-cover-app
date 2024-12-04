1608ae9231e9b748a3cf39eb2b4dbaaf0444a625
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-12-04 11:06:26 +0100

    Clean: Removed useless high mobility bmw data processing in transform.processed_tss.bmw_processed_tss.

--- src/transform/processed_tss/bmw_processed_tss.py
+++ src/transform/processed_tss/bmw_processed_tss.py
@@ -4,7 +4,7 @@ from core.pandas_utils import *
 from core.time_series_processing import compute_charging_n_discharging_masks
 from core.caching_utils import cache_result
 from core.logging_utils import set_level_of_loggers_with_prefix
-from core.console_utils import single_dataframe_script_main
+from core.console_utils import tss_script_main
 from core.config import *
 from transform.fleet_info.main import fleet_info
 from transform.raw_tss.main import get_raw_tss
@@ -15,41 +15,18 @@ logger = getLogger("transform.processed_tss.bmw")
 
 @cache_result(S3_PROCESSED_TSS_KEY_FORMAT.format(brand="bmw"), on="s3")
 def get_processed_tss() -> DF:
-    raw_tss = get_raw_tss("bmw")
-    raw_tss_from_bmw = raw_tss.query("data_provider == 'bmw'")
-    raw_tss_from_high_mobility = raw_tss.query("data_provider == 'high_mobility'")
-    logger.info("Processing BMW raw TSS from raw tss provided by BMW...")
-    tss_from_bmw = (
-        raw_tss_from_bmw
-        .drop(columns=["date"])
-        .pipe(process_raw_tss_provided_by_bmw)
-    )
-    logger.debug("Done.")
-    logger.debug("Processing BMW raw TSS from raw tss provided by high mobility...")
-    tss_from_high_mobility = (
-        raw_tss_from_high_mobility
-        .drop(columns=["date_of_value"])
-        .pipe(hm_process_raw_tss)
-        .pipe(dropna_cols)
-    )
-    logger.debug("Done.")
-    logger.debug(f"Sanity check for processed TSS from BMW:\n{sanity_check(tss_from_bmw)}")
-    logger.debug(f"Sanity check for processed TSS from high mobility:\n{sanity_check(tss_from_high_mobility)}")
-    logger.debug("Concatenating processed TSS from BMW and high mobility...")
-    return pd.concat((tss_from_bmw, tss_from_high_mobility))
-
-def process_raw_tss_provided_by_bmw(tss:DF) -> DF:
-    tss = tss.rename(columns=RENAME_COLS_DICT, errors="ignore")
-    tss = safe_locate(tss, col_loc=list(COL_DTYPES.keys()), logger=logger)
-    tss = safe_astype(tss, COL_DTYPES, logger=logger)
-    tss = tss.sort_values(by=["vin", "date"])
-    tss = left_merge(tss, fleet_info, left_on="vin", right_on="vin", src_dest_cols=COLS_TO_CPY_FROM_FLEET_INFO, logger=logger)
-    tss = compute_charging_n_discharging_masks(tss, id_col="vin", charging_status_val_to_mask=CHARGING_STATUS_VAL_TO_MASK, logger=logger)
-    tss = dropna_cols(tss, logger=logger)
-
-    return tss
+    return (
+        get_raw_tss("bmw")
+        .rename(columns=RENAME_COLS_DICT, errors="ignore")
+        .pipe(safe_locate, col_loc=list(COL_DTYPES.keys()), logger=logger)
+        .pipe(safe_astype, COL_DTYPES, logger=logger)
+        .sort_values(by=["vin", "date"])
+        .merge(fleet_info, left_on="vin", right_on="vin", how="left")
+        .pipe(compute_charging_n_discharging_masks, id_col="vin", charging_status_val_to_mask=CHARGING_STATUS_VAL_TO_MASK, logger=logger)
+        .pipe(dropna_cols, logger=logger)
+    ) 
 
 if __name__ == "__main__":
     set_level_of_loggers_with_prefix("DEBUG", "transform.processed_tss")
-    single_dataframe_script_main(get_processed_tss, force_update=True, logger=logger)
+    tss_script_main(get_processed_tss, force_update=True, logger=logger)
 
