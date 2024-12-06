8d19b31f629c48bee3b877bb12e459a5f1f9b2f4
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-12-06 16:32:32 +0100

    Fix: Adde check in case left_on is not present in lhs in core.sql_utils.right_union_merge_rdb_table.

--- src/core/sql_utils.py
+++ src/core/sql_utils.py
@@ -39,6 +39,8 @@ def right_union_merge_rdb_table(lhs: DF, table: str, left_on: list[str], right_o
     # Set left_on and right_on to list if they are not already
     left_on = [left_on] if isinstance(left_on, str) else left_on
     right_on = [right_on] if isinstance(right_on, str) else right_on
+    # Assert that all the left_on columns are in lhs
+    assert all(col in lhs.columns for col in left_on), f"Not all left_on columns are present in lhs:\nleft_on: {left_on}\nlhs columns: {lhs.columns}"
     # Get rhs table and drop rows with null values in right_on columns to prevent having duplicates down the line
     rhs = pd.read_sql_table(table, con)
     if dropna:
