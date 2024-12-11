314845f7484a95b9df5df3bfe3f325b3111922c1
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-12-11 15:07:00 +0100

    Feat: Added filter to renault results to only account for vins with at least 3 discharges.

--- src/transform/results/renault_results.py
+++ src/transform/results/renault_results.py
@@ -14,6 +14,11 @@ def get_results() -> DF:
         .eval("expected_battery_energy = capacity * soc")
         .eval("soh = battery_energy / expected_battery_energy") 
         .query("~in_discharge & soc > 0.5")
+        .groupby("vin")
+        # Ensure that there are at least 3 discharge period
+        # Since discharge_perf_idx is declared as discharge_perf_mask.diff().cumsum(), it increases per discharge AND charge, i.e 2 per discharge
+        # So we check that the max is superiror or equal to 3 * 2
+        .filter(lambda ts: ts["in_discharge_perf_idx"].max() >= 6)
     )
 
 if __name__ == "__main__":
