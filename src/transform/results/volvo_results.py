6d59c9056cb15a205784267461d7e6e88ea83dbc
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-12-11 18:21:44 +0100

    WIP: Added volvo results script but did not added it to transform.results.main because the results are a little bit weird...

---
+++ src/transform/results/volvo_results.py
@@ -0,0 +1,37 @@
+from logging import Logger, getLogger
+
+import plotly.express as px
+
+from core.pandas_utils import *
+from core.console_utils import main_decorator
+from transform.processed_tss.main import get_processed_tss
+from transform.results.config import *
+
+logger = getLogger("transform.results.volvo_results")
+
+@main_decorator
+def main():
+    df = (
+        get_results()
+        .eval("date = date.dt.date")
+        .groupby(["vin", "date"])
+        .agg({
+            "soh": "median",
+            "odometer": "last",
+        })
+        .reset_index()
+    )
+    print(df)
+    if not df.empty:
+        fig = px.scatter(df, x="date", y="soh", color="vin")
+        fig.show()
+
+def get_results() -> DF:
+    return (
+        get_processed_tss("volvo-cars")
+        .eval("soh = estimated_range / soc / range")
+    )
+
+if __name__ == "__main__":
+    main()
+
