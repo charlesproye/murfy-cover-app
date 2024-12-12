ce4f0c96aba0b0f20256ef2a110a878414c9291d
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-12-12 11:17:32 +0100

    Fix: Fixed mercedes results filtering,

--- src/transform/results/mercedes_results.py
+++ src/transform/results/mercedes_results.py
@@ -4,6 +4,7 @@ import plotly.express as px
 
 from core.pandas_utils import *
 from core.console_utils import main_decorator
+from core.logging_utils import set_level_of_loggers_with_prefix
 from transform.processed_tss.main import get_processed_tss
 from transform.results.config import *
 
@@ -11,6 +12,7 @@ logger = getLogger("transform.results.mercedes_results")
 
 @main_decorator
 def main():
+    set_level_of_loggers_with_prefix("DEBUG", "transform.results")
     df = (
         get_results()
         .dropna(subset=["odometer", "soh"])
@@ -24,7 +26,7 @@ def main():
     )
     print(df)
     if not df.empty:
-        fig = px.line(df, x="date", y="soh", color="vin")
+        fig = px.scatter(df, x="date", y="soh", color="vin")
         fig.show()
 
 def get_results() -> DF:
@@ -33,17 +35,15 @@ def get_results() -> DF:
         .groupby("vin")
         .apply(fill_vars, include_groups=False)
         .reset_index()
-        .query('soc > 0.7')
         .assign(charge_size = lambda df: df.groupby(["vin", "in_charge_idx"]).transform("size"))
-        .query("charge_size > 10")
-        .query("in_charge_perf_mask")
+        .query("soc > 0.7 & charge_size > 10 & ~in_discharge_perf_mask")
         .eval("soh = estimated_range / soc / range")
+        .sort_values(["vin", "date"])
     )
 
 def fill_vars(ts:DF) -> DF:
     return (
         ts
-        .eval("odometer = odometer.ffill().bfill()")
         .eval("soc = soc.ffill().bfill()")
         .eval("estimated_range = estimated_range.ffill().bfill()")
     )
