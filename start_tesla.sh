97080c2bd75e727b8d32a70d3e880323b18dedf3
Théophile Marchand-Arvier <theophile@bib-batteries.fr>
2024-12-16 12:21:17 +0100

    fix: problem when assign, desasign and assign again

--- start_tesla.sh
+++ start_tesla.sh
@@ -44,30 +44,6 @@ ACCOUNTS_JSON='[
     "professional_account": true,
     "code": "d77c548f8b98ca5acbc49e741498a0f1b4784a13152496e22f579f6a67088ad6f296b94f"
   },
-  {
-    "access_token_key": "ACCESS_TOKEN_PA",
-    "refresh_token_key": "REFRESH_TOKEN_PA",
-    "professional_account": false,
-    "vehicle_id": "LRWYGCFS6PC552861"
-  },
-  {
-    "access_token_key": "ACCESS_TOKEN_BEN",
-    "refresh_token_key": "REFRESH_TOKEN_BEN",
-    "professional_account": false,
-    "vehicle_id": "5YJ3E7EB1KF334219"
-  },
-  {
-    "access_token_key": "ACCESS_TOKEN_EGGERMONT",
-    "refresh_token_key": "REFRESH_TOKEN_EGGERMONT",
-    "professional_account": false,
-    "vehicle_id": "5YJ3E7EB7KF474436"
-  },
-  {
-    "access_token_key": "ACCESS_TOKEN_BERNIE",
-    "refresh_token_key": "REFRESH_TOKEN_BERNIE",
-    "professional_account": false,
-    "vehicle_id": "5YJSA7H11EFP63486"
-  }
 ]'
 
 # Run the main.py script with the accounts JSON
