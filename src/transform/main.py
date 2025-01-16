b76458c59977c0fae5b5892195359524a5605145
Martin Vaz <martin.vaz@bib-batteries.fr>
2025-01-16 10:22:25 +0100

    fix: transform/main

--- src/transform/main.py
+++ src/transform/main.py
@@ -50,16 +50,14 @@ def run_entire_pipeline():
         logging.info("Starting pipeline execution")
         start_time = DT.now()
         
-        # update_all_raw_tss()
-        # logging.info("Raw TSS update completed")
+        update_all_raw_tss()
+        logging.info("Raw TSS update completed")
         
-        # #  update_db_vehicle_table()
+        ProcessedTimeSeries.update_all_tss()
+        logging.info("Processed TSS update completed")
 
-        # ProcessedTimeSeries.update_all_tss()
-        # logging.info("Processed TSS update completed")
-
-        # update_vehicle_data_table()
-        # logging.info("Results update completed")
+        update_vehicle_data_table()
+        logging.info("Results update completed")
 
         VehicleInfoProcessor().process_all_vehicles()
         logging.info("Vehicle info update completed")
