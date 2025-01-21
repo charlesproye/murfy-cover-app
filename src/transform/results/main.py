e6e1c28f21878ab8de1dc81922cba38d35fd26ed
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-01-21 14:11:34 +0100

    Rebase: Fixed merge conflict caused by rebase on dev.

--- src/transform/results/main.py
+++ src/transform/results/main.py
@@ -1,6 +1,3 @@
-import warnings
-warnings.filterwarnings("error", category=RuntimeWarning)
-
 from logging import getLogger
 
 from core.sql_utils import *
@@ -10,17 +7,13 @@ from core.console_utils import single_dataframe_script_main
 from core.logging_utils import set_level_of_loggers_with_prefix
 from transform.results.config import *
 from transform.results.ford_results import get_results as get_ford_results
-from transform.results.volvo_results import get_results as get_volvo_results
 from transform.results.tesla_results import get_results as get_tesla_results
 from transform.results.renault_results import get_results as get_renault_results
 from transform.results.mercedes_results import get_results as get_mercedes_results
-from transform.results.odometer_aggregation import agg_last_odometer
-
+from transform.results.volvo_results import get_results as get_volvo_results
 
 logger = getLogger("transform.results.main")
 GET_RESULTS_FUNCS = {
-    "bmw": lambda: agg_last_odometer("bmw"),
-    "kia": lambda: agg_last_odometer("kia"),
     "mercedes-benz": get_mercedes_results,
     "renault": get_renault_results,
     "tesla": get_tesla_results,
@@ -28,8 +21,8 @@ GET_RESULTS_FUNCS = {
     "volvo": get_volvo_results,
 }
 
-def fill_vehicle_data_table_with_results():
-    logger.info("Filling 'vehicle_data' table with results.")
+def update_vehicle_data_table():
+    logger.info("Updating 'vehicle_data' table.")
     return (
         get_all_processed_results()
         .pipe(left_merge_rdb_table, "vehicle", "vin", "vin", {"id": "vehicle_id"})
@@ -45,34 +38,40 @@ def fill_vehicle_data_table_with_results():
 def get_all_processed_results() -> DF:
     return (
         pd.concat([get_processed_results(brand) for brand in GET_RESULTS_FUNCS.keys()])
-        .groupby("vin")
-        .apply(add_lines_up_to_today_for_vehicle, include_groups=False)
-        .reset_index()
-    )
-
-def add_lines_up_to_today_for_vehicle(results:DF) -> DF:
-    last_date = pd.Timestamp.now().floor(UPDATE_FREQUENCY).date()
-    dates_up_to_last_date = pd.date_range(results["date"].min(), last_date, freq=UPDATE_FREQUENCY, name="date")
-    return (
-        results
-        .set_index("date")
-        .sort_index()
-        .reindex(dates_up_to_last_date, method="ffill")
     )
 
 def get_processed_results(brand:str) -> DF:
     results = GET_RESULTS_FUNCS[brand]()
-    logger.info(f"Processing results for {brand}.")
-    if brand != "tesla":
-        results = agg_results_by_update_frequncy(results)
-    return (
+    logger.info(f"Processing {brand} results.")
+    results =  (
         results
-        .dropna(subset=["odometer"], how="any")
-        .reset_index()
+        .sort_values(["vin", "date"])
+        .pipe(agg_results_by_update_frequency)
         .groupby('vin')
         .apply(make_soh_presentable, include_groups=False)
         .reset_index(drop=False)  # Supprime l'index 'vin' créé par le groupby
-        .pipe(set_floored_day_date)
+        .pipe(filter_results_by_lines_bounds, VALID_SOH_LINE_BOUNDS, logger=logger)
+        .groupby("vin")
+        .apply(add_lines_up_to_today_for_vehicle, include_groups=False)
+        .reset_index()
+        .sort_values(["vin", "date"])
+    )
+    results["soh"] = results.groupby("vin")["soh"].ffill()
+    results["odometer"] = results.groupby("vin")["odometer"].ffill()
+    results["soh"] = results.groupby("vin")["soh"].bfill()
+    results["odometer"] = results.groupby("vin")["odometer"].bfill()
+    return results
+
+def agg_results_by_update_frequency(results:DF) -> DF:
+    results["date"] = (
+        pd.to_datetime(results["date"], format='mixed')
+        .dt.floor(UPDATE_FREQUENCY)
+        .dt.tz_localize(None)
+        .dt.date
+        .astype('datetime64[ns]')
+    )
+    return (
+        results
         .groupby(["vin", "date"])
         .agg({
             "odometer": "last",    
@@ -81,22 +80,11 @@ def get_processed_results(brand:str) -> DF:
             "version": "first",
         })
         .reset_index()
-        .pipe(filter_results_by_lines_bounds, VALID_SOH_POINTS_LINE_BOUNDS, logger=logger)
-        .sort_values(["vin", "odometer"])
     )
-
-def set_floored_day_date(df:DF, date_col:str="date") -> DF:
-    df[date_col] = (
-        pd.to_datetime(df[date_col], format='mixed')
-        .dt.floor(UPDATE_FREQUENCY)
-        .dt.tz_localize(None)
-        .dt.date
-        .astype('datetime64[ns]')
-    )
-    return df
 
 def make_soh_presentable(df:DF) -> DF:
     if df["soh"].isna().all():
+        logger.warning(f"No SOH data for {df.name}")
         return df
     if len(df) > 3:
         outliser_mask = mask_out_outliers_by_interquartile_range(df["soh"])
@@ -106,28 +94,19 @@ def make_soh_presentable(df:DF) -> DF:
         df["soh"] = force_monotonic_decrease(df["soh"])
     return df
 
-def agg_results_by_update_frequncy(results:DF) -> DF:
-    """
-    Some results have the same amount of rows as the make's time series length.
-    So we aggregate the results by update frequency to prevent memory overload.
-    """
-    return (
+def add_lines_up_to_today_for_vehicle(results:DF) -> DF:
+    last_date = pd.Timestamp.now().floor(UPDATE_FREQUENCY).date()
+    dates_up_to_last_date = pd.date_range(results["date"].min(), last_date, freq=UPDATE_FREQUENCY, name="date")
+    test = (
         results
-        .eval("floored_date = date.dt.floor(@UPDATE_FREQUENCY)")
-        .groupby(["vin", "floored_date"])
-        .agg({
-            "soh": "median",
-            "odometer": "last",
-            "date": "last",
-            "date": "first",
-            "model": "first",
-            "version": "first",
-        })
-        .reset_index()
+        .set_index("date")
+        .sort_index()
+        .reindex(dates_up_to_last_date, method="ffill")
     )
+    return test
 
 if __name__ == "__main__":
     set_level_of_loggers_with_prefix("DEBUG", "core.sql_utils")
     set_level_of_loggers_with_prefix("DEBUG", "transform.results")
-    single_dataframe_script_main(fill_vehicle_data_table_with_results)
+    single_dataframe_script_main(update_vehicle_data_table)
 
