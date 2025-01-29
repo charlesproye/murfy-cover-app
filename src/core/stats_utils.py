722f04865162b0a5807c5471a34b786ef5215701
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-01-29 17:47:41 +0100

    Fix: Fixed force_monotonic_decrease to work with NaNs (again).

--- src/core/stats_utils.py
+++ src/core/stats_utils.py
@@ -25,7 +25,6 @@ def filter_results_by_lines_bounds(results: DF, valid_soh_points: DF, logger: Lo
     if results["soh"].isna().all():
         logger.debug("No SoH values to filter, column is all NaN returning as is.")
         return results
-    logger.debug("Filtering results.")
     max_intercept, max_slope = intercept_and_slope_from_points(valid_soh_points.xs("max", level=0, drop_level=True))
     min_intercept, min_slope = intercept_and_slope_from_points(valid_soh_points.xs("min", level=0, drop_level=True))
     results = (
@@ -40,7 +39,7 @@ def filter_results_by_lines_bounds(results: DF, valid_soh_points: DF, logger: Lo
     if results.shape[0]:
         rows_removed_pct = 100 * nb_rows_removed / results.shape[0]
         if nb_rows_removed == results.shape[0]:
-            logger.warning(f"All SoH results were set to NaN, check the valid SOH points.")
+            logger.warning(f"While filtering, all SoH results were set to NaN, check the valid SOH points.")
         else:
             logger.debug(f"Filtered SoH results, {nb_rows_removed}({rows_removed_pct:.2f}%) set to NaN.")
     else: 
@@ -68,17 +67,16 @@ def force_monotonic_decrease(values:Series) -> Series:
     l'écart avec les valeurs brutes.
     """
     values = values.copy()
-    notna_values = values[values.notna()]
-    n = len(notna_values)
+    notna_values = values.dropna()
+    nb_vals = notna_values.size
     # Fonction objectif : minimiser l'écart quadratique entre les valeurs ajustées et les valeurs brutes
     def objectif(adjusted_values:np.ndarray) -> float:
         return np.sum((adjusted_values - notna_values) ** 2)
    # Contraintes : SoH doit être non-croissant et l'écart hebdomadaire doit être inférieur à 0,1%
-    # constraints = []
-    constraints = [{'type': 'ineq', 'fun': lambda x, i=i: (x[i - 1] - x[i]) * 0.00001} for i in range(1, n)] 
-    constraints += [{'type': 'ineq', 'fun': lambda x, i=i: 0.00005 * x[i - 1] - (x[i - 1] - x[i])} for i in range(1, n)]
+    constraints = [{'type': 'ineq', 'fun': lambda x, i=i: (x[i - 1] - x[i]) * 0.00001} for i in range(1, nb_vals)] 
+    constraints += [{'type': 'ineq', 'fun': lambda x, i=i: 0.00005 * x[i - 1] - (x[i - 1] - x[i])} for i in range(1, nb_vals)]
     # Borne supérieure et inférieure (par exemple, entre 0 et 100)
-    bounds = [(0, 100)] * n
+    bounds = [(0, 100)] * nb_vals
     # Initialisation des valeurs ajustées (on commence par les valeurs brutes)
     # Résolution de l'optimisation
     results = minimize(objectif, notna_values, constraints=constraints, bounds=bounds, method="SLSQP")
