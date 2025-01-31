82428d048f0057ba573229754da3110d88d8a786
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-01-31 11:25:34 +0100

    Feat: Increased the SoH upbound of tesla results from 1 to 1.05.

--- src/transform/results/tesla_results.py
+++ src/transform/results/tesla_results.py
@@ -73,7 +73,7 @@ def get_results() -> DF:
         .reset_index(drop=False)
         .eval("energy_added = energy_added_end - energy_added_min")
         .eval("soh = energy_added / (soc_diff / 100.0 * capacity)")
-        .query("soc_diff > 40 & soh.between(0.75, 1.0)")
+        .query("soc_diff > 40 & soh.between(0.75, 1.05)")
         .eval("level_1 = soc_diff * (charging_power < @LEVEL_1_MAX_POWER) / 100")
         .eval("level_2 = soc_diff * (charging_power.between(@LEVEL_1_MAX_POWER, @LEVEL_2_MAX_POWER)) / 100")
         .eval("level_3 = soc_diff * (charging_power > @LEVEL_2_MAX_POWER) / 100")
