697b1cbcd16651667a6067d7ffc2317f48f3b675
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-02-03 11:17:25 +0100

    Feat: Wrote a simple transfrom.raw_results.main to update all the raw results.

---
+++ src/transform/raw_results/main.py
@@ -0,0 +1,25 @@
+from core.console_utils import main_decorator
+from core.logging_utils import set_level_of_loggers_with_prefix
+from transform.raw_results.ford_results import get_results as ford_results
+from transform.raw_results.mercedes_results import get_results as mercedes_results
+from transform.raw_results.renault_results import get_results as renault_results
+from transform.raw_results.tesla_results import get_results as tesla_results
+from transform.raw_results.volvo_results import get_results as volvo_results
+
+def update_all_raw_tss():
+    ford_results(force_update=True)
+    mercedes_results(force_update=True)
+    renault_results(force_update=True)
+    tesla_results(force_update=True)
+    volvo_results(force_update=True)
+
+
+@main_decorator
+def main():
+    print("Updating all raw results...")
+    set_level_of_loggers_with_prefix("DEBUG", "transform.raw_results")
+    update_all_raw_tss()
+
+if __name__ == "__main__":
+    main()
+
