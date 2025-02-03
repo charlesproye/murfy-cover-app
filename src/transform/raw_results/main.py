e08a70f7fe64f08d02b916725b89c2054b035ff5
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-02-03 11:32:39 +0100

    Fix: Added odometer aggregation for brands that we don't have an SoH estimation for (yet) in transfrom.raw_results.main.update_all_raw_results.

--- src/transform/raw_results/main.py
+++ src/transform/raw_results/main.py
@@ -1,10 +1,12 @@
 from core.console_utils import main_decorator
 from core.logging_utils import set_level_of_loggers_with_prefix
+from transform.raw_results.odometer_aggregation import agg_last_odometer
 from transform.raw_results.ford_results import get_results as ford_results
-from transform.raw_results.mercedes_results import get_results as mercedes_results
-from transform.raw_results.renault_results import get_results as renault_results
 from transform.raw_results.tesla_results import get_results as tesla_results
 from transform.raw_results.volvo_results import get_results as volvo_results
+from transform.raw_results.renault_results import get_results as renault_results
+from transform.raw_results.mercedes_results import get_results as mercedes_results
+from transform.processed_results.config import *
 
 def update_all_raw_tss():
     ford_results(force_update=True)
@@ -12,7 +14,8 @@ def update_all_raw_tss():
     renault_results(force_update=True)
     tesla_results(force_update=True)
     volvo_results(force_update=True)
-
+    for make in MAKES_WITHOUT_SOH:
+        agg_last_odometer(make, force_update=True)
 
 @main_decorator
 def main():
