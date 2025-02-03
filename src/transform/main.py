dfa8ba7b7ce79f12fb6b67f9f5f76a45ab898861
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-02-03 11:35:42 +0100

    Feat: Added transform.raw_results.main.update_all_raw_results into transform.main.run_entire_pipeline.

--- src/transform/main.py
+++ src/transform/main.py
@@ -10,16 +10,14 @@ from apscheduler.triggers.interval import IntervalTrigger
 from core.console_utils import main_decorator, parse_kwargs
 from core.logging_utils import set_level_of_loggers_with_prefix
 from transform.config import *
-
 from transform.raw_tss.main import update_all_raw_tss
 from transform.processed_tss.ProcessedTimeSeries import ProcessedTimeSeries
-from transform.results.main import update_vehicle_data_table
+from transform.raw_results.main import update_all_raw_tss
+from transform.processed_results.main import update_vehicle_data_table
 from transform.vehicle_info.main import VehicleInfoProcessor
 from transform.front_utils.main import FrontUtils
 
-
 logger = getLogger("transform.main")
-
 
 @main_decorator
 def main():
@@ -54,8 +52,8 @@ def run_entire_pipeline():
         VehicleInfoProcessor().process_all_vehicles()
         logging.info("Vehicle info update completed")
         
-        # update_all_raw_tss()
-        # logging.info("Raw TSS update completed")
+        update_all_raw_tss()
+        logging.info("Raw TSS update completed")
         
         # ProcessedTimeSeries.update_all_tss()
         # logging.info("Processed TSS update completed")
