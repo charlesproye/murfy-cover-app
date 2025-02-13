e876ad3e0570e4f56ffd2db361ec877a7ff1972a
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-02-13 18:06:00 +0100

    Feat: Added new transformraw_results.get_tesla_soh_readouts module to facilitate the data extraction of the aviloo soh readouts.

---
+++ src/transform/raw_results/get_tesla_soh_readouts.py
@@ -0,0 +1,27 @@
+"""
+We recieved a report of Aviloo SoH estimations from Ayvens on some Teslas.  
+This module facilitates the obtention of these results.  
+"""
+from core.pandas_utils import *
+
+READOUTS_DF_PATH = "data_cache/aviloo_soh_readouts.csv"
+
+def get_aviloo_soh_readouts() -> DF:
+    return (
+        pd.read_csv(
+            READOUTS_DF_PATH,
+            dtype={
+                "Score Aviloo": "int64",
+                "SoH Readout": "float64",
+                "VIN": "string",
+                "BIB SOH": "float64",
+                "Brand (FlashTest)": "string",
+                "Model Group (FlashTest)": "string",
+                "Mileage": "float64",
+            }
+        )
+        .rename(columns={"VIN": "vin", "SoH Readout": "soh_readout"})
+    )
+
+if __name__ == "__main__":
+    print(get_aviloo_soh_readouts())
