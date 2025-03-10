73bc80fbaaeed6fa89f442c1d363c951b1592b69
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-03-10 19:01:25 +0100

    Feat: Refactored transform.raw_tss.tesla_raw_tss to use more multi threading for parsing the responses and save subsets (one week each) of raw_tss.

--- src/transform/raw_tss/tesla_raw_tss.py
+++ src/transform/raw_tss/tesla_raw_tss.py
@@ -1,47 +1,73 @@
-from logging import Logger, getLogger
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from rich.progress import Progress
+from os.path import splitext
+from concurrent.futures import ThreadPoolExecutor
+
+from logging import getLogger
+from rich.progress import track
 
 from core.pandas_utils import *
-from transform.raw_tss.config import *
-from core.logging_utils import set_level_of_loggers_with_prefix
 from core.s3_utils import S3_Bucket
+from transform.raw_tss.config import *
 from core.caching_utils import cache_result
+from core.console_utils import main_decorator
+from core.logging_utils import set_level_of_loggers_with_prefix
+
 
 logger = getLogger(f"transform.Tesla-RawTSS")
 
-@cache_result(S3_RAW_TSS_KEY_FORMAT.format(brand="tesla"), on="s3")
+@main_decorator
+def main():
+    set_level_of_loggers_with_prefix("DEBUG", "transform")
+    print(sanity_check(get_raw_tss(force_update=True)))
+
+@cache_result(TESLA_RAW_TSS_KEY, on="s3")
 def get_raw_tss(bucket: S3_Bucket = S3_Bucket()) -> DF:
     logger.debug("Getting raw tss from responses provided by tesla.")
-    keys = bucket.list_responses_keys_of_brand("tesla")
-    
-    results = []
-    with Progress() as progress:
-        task = progress.add_task("Parsing responses...", visible=False, total=len(keys))
-        with ThreadPoolExecutor(max_workers=10) as executor:  # Adjust max_workers based on your system's capacity
-            futures = {
-                executor.submit(parse_response_as_raw_ts, key, bucket, logger, progress, task): key
-                for _, key in keys.iterrows()
-            }
+    keys = get_response_keys_to_parse(bucket)
+    new_raw_tss = get_raw_tss_from_keys(keys, bucket)
+    if bucket.check_file_exists(TESLA_RAW_TSS_KEY):
+        return concat([bucket.read_parquet_df(TESLA_RAW_TSS_KEY), new_raw_tss])
+    else:
+        return new_raw_tss
 
-            for future in as_completed(futures):
-                result = future.result()
-                if not result.empty:
-                    results.append(result)
+def get_response_keys_to_parse(bucket:S3_Bucket) -> DF:
+    if bucket.check_file_exists(TESLA_RAW_TSS_KEY):
+        raw_tss_subset = bucket.read_parquet_df(TESLA_RAW_TSS_KEY, columns=["vin", "readable_date"])
+    else:
+        raw_tss_subset = DEFAULT_TESLA_RAW_TSS_DF
+    last_parsed_date = (
+        raw_tss_subset
+        .groupby("vin", observed=True, as_index=False)
+        # Use "max" instead of "last" as the keys are not sorted
+        .agg(last_parsed_date=pd.NamedAgg("readable_date", "max"))
+    )
+    return (
+        pd.read_parquet("tesla_response_keys.parquet") #bucket.list_responses_keys_of_brand("tesla")
+        .assign(date=lambda df: df["file"].str[:-5].astype("datetime64[ns]"))
+        .merge(last_parsed_date, "outer", "vin")
+        .query("last_parsed_date.isna() | date > last_parsed_date") 
+    )
 
-    return concat(results)
-
-def parse_response_as_raw_ts(key: Series, bucket: S3_Bucket, logger: Logger, progress: Progress, task: int) -> DF:
-    progress.update(task, visible=True, advance=1, description=f"{key['vin']} {key['file']}")
-    response = bucket.read_json_file(key["key"])
-    if response is None:
-        logger.debug(f"Did not parse key {key['key']} because the object returned by read_json_file was None.")
-        return DF()
-    raw_ts = DF.from_records(response)
-    raw_ts["vin"] = key["vin"]
-
-    return raw_ts
+def get_raw_tss_from_keys(keys:DF, bucket:S3_Bucket) -> DF:
+    raw_tss = []
+    for week, week_keys in track(keys.groupby(pd.Grouper(key='date', freq='W-MON'))):
+        week_date = week.date().strftime('%Y-%m-%d')
+        logger.debug(f"Parsing the responses of the week {week_date}:")
+        logger.debug(f"{len(week_keys)} keys to parse for {week_keys['vin'].nunique()} vins.")
+        logger.debug(f"This represents {len(week_keys) / len(keys):.2f}% of the total keys to parse.")
+        responses = bucket.read_multiple_json_files(week_keys["key"].tolist(), max_workers=64)
+        logger.debug(f"Read the responses.")
+        with ThreadPoolExecutor(max_workers=64) as executor:
+            week_raw_tss = list(executor.map(DF.from_records, responses))
+        logger.debug(f"Parsed the responses:")
+        week_raw_tss = concat(filter(lambda df: not df.empty, week_raw_tss))
+        logger.debug(f"Concatenated the responses into a single DF.")
+        base, ext = splitext(TESLA_RAW_TSS_KEY)
+        bucket.save_df_as_parquet(week_raw_tss, f"{base}_{week_date}{ext}")
+        logger.debug(f"Saved the DF at '{base}_{week_date}{ext}'.")
+        raw_tss.append(week_raw_tss)
+        logger.debug("")
+    return concat(raw_tss, ignore_index=True) 
 
 if __name__ == "__main__":
-    set_level_of_loggers_with_prefix("DEBUG", "transform")
-    get_raw_tss(force_update=True)
+    main()
+
