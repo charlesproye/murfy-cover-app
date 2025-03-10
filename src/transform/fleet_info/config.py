ccb147287c2b69221d4ee67d27868250a8707725
Martin Vaz <martin.vaz@bib-batteries.fr>
2025-03-10 18:52:11 +0100

    wip

--- src/transform/fleet_info/config.py
+++ src/transform/fleet_info/config.py
@@ -3,6 +3,7 @@ from pandas.api.types import CategoricalDtype
 TABLE_QUERY = """
 SELECT * FROM vehicle
 JOIN vehicle_model ON vehicle.vehicle_model_id = vehicle_model.id
+JOIN battery ON vehicle_model.battery_id = battery.id 
 JOIN oem ON vehicle_model.oem_id = oem.id
 JOIN fleet ON vehicle.fleet_id = fleet.id
 JOIN region ON vehicle.region_id = region.id
