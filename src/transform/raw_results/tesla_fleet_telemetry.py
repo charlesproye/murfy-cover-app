fix charging_power computation

src/transform/raw_results/tesla_fleet_telemetry.py
@@ -18,7 +18,7 @@ def main():
     set_level_of_loggers_with_prefix("DEBUG", "transform.raw_results")
     results = get_results(force_update=True)
 
-@cache_result(RAW_RESULTS_CACHE_KEY_TEMPLATE.format(make="tesla-fleet-telemetry"), "s3")
+#@cache_result(RAW_RESULTS_CACHE_KEY_TEMPLATE.format(make="tesla-fleet-telemetry"), "s3")
 def get_results() -> DF:
     logger.info("Processing raw tesla fleet telemetry results.")
     results = (TeslaProcessedTimeSeries('tesla-fleet-telemetry',)
@@ -40,7 +40,7 @@ def get_results() -> DF:
             dc_charging_power=pd.NamedAgg("dc_charging_power", "median"),
             tesla_code=pd.NamedAgg("tesla_code", "first"),
         )
-        .eval("charging_power = ac_charging_power + dc_charging_power")
+        .assign(charging_power = lambda df: df['ac_charging_power'].replace([np.nan, -np.nan], 0) + df['dc_charging_power'].replace([np.nan, -np.nan], 0))
         .eval("ac_energy_added = ac_energy_added_end  - ac_energy_added_min")
         .eval("dc_energy_added = dc_energy_added_end  - dc_energy_added_min")
         .assign(energy_added=lambda df: np.maximum(df["ac_energy_added"].replace([np.nan, -np.nan], 0), df["dc_energy_added"].replace([np.nan, -np.nan], 0)))