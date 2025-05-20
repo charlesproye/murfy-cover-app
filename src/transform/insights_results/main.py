8dffed9777591cc22d10d787bbb385e7b082ef49
hugochant <hugo.c@club-internet.fr>
2025-05-20 17:01:16 +0200

    update with gsheet utils files

--- src/transform/insights_results/main.py
+++ src/transform/insights_results/main.py
@@ -1,10 +1,11 @@
 from transform.insights_results.update_scoring import main 
-from transform.insights_results.trendlines import run_trendline_main 
-from transform.insights_results.trendlines_excel import run_excel_trendlines 
-
-
+from transform.insights_results.trendlines import run_trendline_main
+from transform.insights_results.trendlines_excel import run_excel_trendlines
+from core.gsheet_utils import clean_gsheet
 
 if __name__ == "__main__":
-    main()
-    run_excel_trendlines()
+    # main()
+    clean_gsheet("BP - Rapport Freemium", "Trendline")
     run_trendline_main()
+    run_excel_trendlines()
+
