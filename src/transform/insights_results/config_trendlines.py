2155fda0f4047d29440fab57f79c1b2911f12373
hugochant <hugo.c@club-internet.fr>
2025-06-03 10:18:00 +0200

    add mapping

--- src/transform/insights_results/config_trendlines.py
+++ src/transform/insights_results/config_trendlines.py
@@ -2,4 +2,49 @@
 
 TRENDLINE_MODEL = {}
 
+TYPE_MAPPING = {
+##### Renault #####
+    # R110 
+    "E-Tech Equilibre charge normale R110": "R110",
+    "E-Tech Equilibre charge normale R110 Achat Intégral": "R110",
+    "E-Tech Evolution charge normale R110": "R110",
+    "E-Tech Intens charge normale R110": "R110",
+    "E-Tech Life charge normale R110": "R110",
+    "E-Tech Life charge normale R110 Achat Intégral": "R110",
+    "E-Tech Limited charge normale R110": "R110",
+    "E-Tech Limited charge normale R110 Achat Intégral": "R110",
+    "R110 Business": "R110",
+    "R110 Life": "R110",
+    
+    # R135
+    "E-Tech Techno charge normale R135": "R135",
 
+#### Peugeot ####
+
+
+#### Citroën ####
+    "Moteur électrique 136ch (100 kW) Feel Pack Automatique": "C4 100 kW",
+    "Moteur électrique 136ch (100 kW) Shine Automatique": "C4 100 kW",
+
+#### Fiat ####
+
+#### DS ####
+
+#### Dacia ####
+
+
+#### BMW ####
+
+#### Kia ####
+
+#### Hyundai ####
+
+#### Nissan ####
+
+#### Mercedes ####
+
+#### Mini ####
+
+#### Volkswagen ####
+
+}
