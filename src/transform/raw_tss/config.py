cce74cfece36d007a49cc16480c7f4ca02447b6b
hugochant <hugo.c@club-internet.fr>
2025-06-20 17:00:55 +0200

    update config

--- src/transform/raw_tss/config.py
+++ src/transform/raw_tss/config.py
@@ -1,4 +1,6 @@
 from pandas import DataFrame as DF
+from transform.raw_tss.parsing import * 
+
 
 S3_RAW_TSS_KEY_FORMAT = "raw_ts/{brand}/time_series/spark_raw_tss.parquet"
 TESLA_RAW_TSS_KEY = S3_RAW_TSS_KEY_FORMAT.format(brand="tesla")
@@ -7,3 +9,26 @@ DEFAULT_TESLA_RAW_TSS_DF = DF(columns=["vin", "readable_date"])
 FLEET_TELEMETRY_RAW_TSS_KEY = S3_RAW_TSS_KEY_FORMAT.format(brand="tesla-fleet-telemetry")
 
 SPARK_FLEET_TELEMETRY_RAW_TSS_KEY = S3_RAW_TSS_KEY_FORMAT.format(brand="tesla-fleet-telemetry")
+
+ALL_MAKES = ["tesla-fleet-telemetry",
+             "bmw",
+             "mercedes-benz",
+             "ford",
+             "volvo-cars",
+             "stellantis",
+             "kia",
+             "renault",
+]
+
+LIST_COL_TO_DROP = ["model"]
+
+GET_PARSING_FUNCS = {
+    #"bmw": ,
+    #"stellantis": ,
+    "mercedes-benz": parse_high_mobility,
+    "kia": parse_high_mobility,
+    "renault": parse_high_mobility,
+    "volvo": parse_high_mobility,
+    "ford": parse_high_mobility,
+    "tesla-fleet-telemetry": parse_fleet_telemetry 
+}
