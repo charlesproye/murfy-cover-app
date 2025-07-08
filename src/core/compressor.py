98e5b1236f157053ecb9314692ea56ae5c1ac34d
Max <maxime@ynstant.io>
2025-07-08 13:59:41 +0200

    accelerate compression and add logs

--- src/core/compressor.py
+++ src/core/compressor.py
@@ -1,6 +1,5 @@
 import asyncio
 
-import msgspec
 from src.core.s3.async_s3 import AsyncS3
 from datetime import datetime
 from abc import ABC, abstractmethod
@@ -15,16 +14,23 @@ class Compressor(ABC):
     @abstractmethod
     def brand_prefix(self)->str:
         pass
-
+    
     async def run(self):
         vins_folders, _ = await self._s3.list_content(f"response/{self.brand_prefix}/")
+        r = await asyncio.gather(*(self._s3.list_content(f"{vin_path}temp/") for vin_path in vins_folders))
+        vins_with_temps:list[str] = []
+        for i,(_, files) in enumerate(r): 
+            if len(files)>0:
+                vins_with_temps.append(vins_folders[i])
         print(f"{vins_folders = }")
-        await asyncio.gather(
-            *(self.compress_temp_vin_data(vin_path) for vin_path in vins_folders)
-        )
+        print(f"{vins_with_temps = }")
+        for vin_path in vins_with_temps:
+            await self.compress_temp_vin_data(vin_path)
 
     async def compress_temp_vin_data(self, vin_folder_path: str):
+        print(f"DOWNLOAD VIN: {vin_folder_path}")
         new_files = await self._s3.download_folder(f"{vin_folder_path}temp/")
+        print(f"{vin_folder_path}: {len(new_files) = }")
         if len(new_files) == 0:
             return
         encoded_data = self.temp_data_to_daily_file(new_files)
