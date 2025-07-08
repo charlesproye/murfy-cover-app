48870497c55cbc4c7570ebf6b35053b4603cc986
hugochant <hugo.c@club-internet.fr>
2025-07-08 10:46:09 +0200

    remove old files

--- src/transform/insights_results/main.py
+++ src/transform/insights_results/main.py
@@ -6,6 +6,4 @@ from core.gsheet_utils import clean_gsheet
 if __name__ == "__main__":
     # main()
     clean_gsheet("BP - Rapport Freemium", "Trendline")
-    run_trendline_main()
-    run_excel_trendlines()
 
