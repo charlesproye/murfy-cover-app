aa81e4b50b616eb371be61eaf488313b37d13a0c
Charles Proye <charles.proye@gmail.com>
2025-07-15 16:10:54 +0200

    config BMW

--- src/transform/raw_tss/config.py
+++ src/transform/raw_tss/config.py
@@ -547,16 +547,19 @@ SCHEMAS = {
                     ]))
                 ]),
     "bmw": StructType([
-                StructField("vin", StringType(), True),
-                StructField("pushKeyValues", ArrayType(
-                    StructType([
-                        StructField("key", StringType(), True),
-                        StructField("value", StringType(), True),  # ou DoubleType() si tu veux caster ensuite
-                        StructField("unit", StringType(), True),
-                        StructField("info", StringType(), True),
-                        StructField("date_of_value", TimestampType(), True)  # à condition que tu passes le format ISO-8601
-                    ])
-                ), True)
+                StructField("data", StructType([
+                    StructField("vin", StringType(), True),
+                    StructField("pushKeyValues", ArrayType(
+                        StructType([
+                            StructField("key", StringType(), True),
+                            StructField("value", StringType(), True),  # ou DoubleType() si tu veux caster ensuite
+                            StructField("unit", StringType(), True),
+                            StructField("info", StringType(), True),
+                            StructField("date_of_value", TimestampType(), True)  # à condition que tu passes le format ISO-8601
+                        ])
+                    ), True)
+                ])
+                )
             ])
 }
 
