e3c5094c0e0720ed2268c02bc7aa1bf6ebf6ff76
hugochant <hugo.c@club-internet.fr>
2025-07-16 11:23:10 +0200

    update

--- src/load/trendline/main.py
+++ src/load/trendline/main.py
@@ -2,67 +2,68 @@
 import logging
 import numpy as np
 import pandas as pd
-
 from sqlalchemy import text
-
 from load.trendline.trendline_utils import *
 from core.gsheet_utils import *
 from rapidfuzz import process, fuzz
-def uniform_vehicules_type(type_car, oem_name, model_name, db_df, battery_capacity=None):
-    """Permet d'uniformiser les types de véhicules avec ceux présent dans la db 
+import re
 
+def mapping_vehicle_type(type_car, oem_name, model_name, db_df, battery_capacity=None):
+    """Map a given vehicle to the closest model identifier in the database.
     Args:
-        row (pd.Series): avec les infos du vin min required column: oem, Modèle, Type 
-        db_df (pd.DataFrame): dataframe avec les colonnes model_name, id, type, oem_name, capacity
+        type_car (str): type car to find match
+        oem_name (str): oem car
+        model_name (str): model car
+        db_df (pd.DataFrame): db with all the model in dbeaver
+        battery_capacity (str, optional): capacity car battery. Defaults to None.
 
     Returns:
-        str: type du modèle présent sur dbeaver
+        str: type le plus proche présent dans la db de vehicle_model
     """
-
-#__________ Faire tourner cette requête en dehors pour récupérer les infos nécessaires sur la db_________
-# from core.sql_utils import *
-# engine = get_sqlalchemy_engine()
-# con = engine.connect()
-
-# with engine.connect() as connection:
-#     dbeaver_df = pd.read_sql(text("""SELECT vm.model_name, vm.id, vm.type, o.oem_name, b.capacity FROM vehicle_model vm
-#                                   join OEM o on vm.oem_id=o.id
-#                                   join battery b on b.id=vm.battery_id;"""), con)
-#___________________________________________________________________________________________________________
-
-    
-    #On récupère les infos
+   
     oem_name = oem_name.lower()
-    model_name = model_name.lower()
     type_car = type_car.lower()
-    # filtre sur l'oem 
+    try:
+        if len(model_name) > 4:
+            d = re.findall('\d*', model_name)
+            d.sort()
+            model_name = d[-1]
+    except:
+        model_name = model_name.lower()
+        
+    # filtre sur l'oem
     subset = db_df[db_df['oem_name'] == oem_name].copy()
+
     # Trouver la meilleure correspondance
-    # Retourne le modèle le plus proche score_cutoff fixé a 0 pour le moment pour être sur d'avoir un retour
-    match_model = process.extractOne(model_name, subset['model_name'], scorer=fuzz.token_sort_ratio)
+    # Retourne le modèle le plus proche score_cutoff fixé à 0.1 pour le moment pour être presque sur d'avoir un retour
+    match_model = process.extractOne(model_name, subset['model_name'], scorer=fuzz.token_sort_ratio, score_cutoff=.1)
     if match_model :
-        match_model_name, score, index = match_model
+        match_model_name, _, _ = match_model
         # filtre sur le nom du modèle
         subset = subset[subset['model_name']==match_model_name]
-        # on cherche la batetrie avec la capacité la + proche
+        # on cherche la batterie avec la capacité la + proche
         try:
-            battery_target = float(battery_capacity.replace('kWh', '').replace('kwh', '').strip())
-            subset["distance"] = (subset["capacity"] - battery_target).abs()
-            min_distance = subset["distance"].min()
-            closest_rows = subset[subset["distance"] == min_distance]
-            # Si +sieurs batterie -> type le plus ressemblant
+            if battery_capacity:
+                battery_target = float(battery_capacity.lower().replace('kwh', '').strip())
+                subset["distance"] = (subset["capacity"] - battery_target).abs()
+                closest_rows = subset[subset["distance"] == subset["distance"].min()]
+            else:
+                closest_rows = subset
+
+            # Correspondance floue sur le type
             match_type = process.extractOne(type_car, closest_rows['type'], scorer=fuzz.token_sort_ratio)
-            match_model_type, score, index = match_type
-            return closest_rows.loc[index, "type"]
-        
-        # type le plus ressemblant sans batterie 
+            if match_type:
+                _, _, index = match_type
+                return closest_rows.iloc[index]["type"]
+
+        # type le plus ressemblant sans batterie
         except:
             match_type = process.extractOne(type_car, subset['type'], scorer=fuzz.token_sort_ratio)
-            match_model_type, score, index = match_type
+            _, _, index = match_type
             return subset.loc[index, "type"]
         
-    else:
-        return None
+    raise Exception("unknown model")
+
 def generate_trendline_functions(df, odometer_column, soh_column):
     """_summary_
 
@@ -113,7 +114,7 @@ if __name__ == "__main__":
                                     join battery b on b.id=vm.battery_id;"""), con)
 
     ### Matching type 
-    df_sheet['type'] = df_sheet.apply(lambda row: uniform_vehicules_type(row['Type'], row['OEM'], str(row['Modèle']), dbeaver_df,  row['battery_capacity']), axis=1)
+    df_sheet['type'] = df_sheet.apply(lambda row: mapping_vehicle_type(row['Type'], row['OEM'], str(row['Modèle']), dbeaver_df,  row['battery_capacity']), axis=1)
     
     df_sheet['Modèle'] = df_sheet['Modèle'].apply(lambda x: x.lower())
     df_merge = df_sheet.merge(dbeaver_df[['model_name', "type", 'battery_id']], left_on=['Modèle', "type"], right_on=['model_name', 'type'])
