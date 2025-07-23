a72597944fb367e52ccdfd940e8d713be9e32b6c
hugochant <hugo.c@club-internet.fr>
2025-07-23 11:40:59 +0200

    update

--- src/core/gsheet_utils.py
+++ src/core/gsheet_utils.py
@@ -4,22 +4,46 @@ from google.oauth2.service_account import Credentials
 import numpy as np
 import json 
 import base64
+import logging
+from logging import getLogger
+from typing import Any, List
 
-def get_gspread_client():
-    base64_creds = os.getenv("GOOGLE_PRIVATE_KEY")
-    if not base64_creds:
-        raise ValueError("GOOGLE_PRIVATE_KEY not found in .env")
 
-    creds_dict = json.loads(base64.b64decode(base64_creds))
-    creds = Credentials.from_service_account_info(
-        creds_dict,
-        scopes=["https://www.googleapis.com/auth/spreadsheets", 
-                "https://www.googleapis.com/auth/drive"]
-    )
-    return gspread.authorize(creds)
+logger = getLogger("ingestion.vehicle_info")
+
+def get_google_client() -> Any:
+    """Get authenticated Google Sheets client.
+
+    Returns:
+        gspread.Client: Authenticated Google Sheets client
+    """
+    try:
+        base64_creds = os.getenv("GOOGLE_PRIVATE_KEY")
+        if not base64_creds:
+            raise ValueError("GOOGLE_PRIVATE_KEY not found in environment variables")
+
+        # Décoder et parser les credentials
+        creds_dict = json.loads(base64.b64decode(base64_creds))
+
+        scopes = [
+            'https://www.googleapis.com/auth/spreadsheets',
+            'https://www.googleapis.com/auth/drive'
+        ]
+
+        credentials = Credentials.from_service_account_info(
+            creds_dict,
+            scopes=scopes
+        )
+
+        return gspread.authorize(credentials)
+
+        
+    except Exception as e:
+        logging.error(f"Failed to get Google Sheets client: {str(e)}")
+        raise
 
 def clean_gsheet(gsheet, feuille, keep_first_line=True):
-    client = get_gspread_client()
+    client = get_google_client()
 
     spreadsheet = client.open(gsheet)
     worksheet = spreadsheet.worksheet(feuille)
@@ -44,7 +68,7 @@ def load_excel_data(client, gsheet, feuille):
     return sheet_data
 
 def export_to_excel(df_to_write, gsheet, feuille):
-    client = get_gspread_client()
+    client = get_google_client()
     sheet_out = client.open(gsheet)
     worksheet = sheet_out.worksheet(feuille)
     worksheet.append_rows(df_to_write.values.tolist())
