b02919e892d8e51682684eb558d27cf84f62e06a
hugochant <hugo.c@club-internet.fr>
2025-07-23 11:44:20 +0200

    drop unused files

--- src/activation/fleet_info.py
+++ src/activation/fleet_info.py
@@ -9,14 +9,15 @@ from gspread import Cell
 import re
 
 
-from core.pandas_utils import *
-from core.s3.s3_utils import S3Service
-from core.singleton_s3_bucket import S3
-from core.config import *
-from activation.config.credentials import SPREADSHEET_ID 
-from activation.config.mappings import OEM_MAPPING, COUNTRY_MAPPING, COL_DTYPES, suffixes_to_remove, mappings
-from activation.config.settings import MAX_RETRIES, INITIAL_RETRY_DELAY, MAX_RETRY_DELAY
+from src.core.pandas_utils import *
+from src.core.s3.s3_utils import S3Service
+from src.core.singleton_s3_bucket import S3
+from src.core.config import *
 from src.core.gsheet_utils import get_google_client
+from .config.credentials import SPREADSHEET_ID 
+from .config.mappings import OEM_MAPPING, COUNTRY_MAPPING, COL_DTYPES, suffixes_to_remove, mappings
+from .config.settings import MAX_RETRIES, INITIAL_RETRY_DELAY, MAX_RETRY_DELAY
+
 
 logger = getLogger("ingestion.vehicle_info")
  
