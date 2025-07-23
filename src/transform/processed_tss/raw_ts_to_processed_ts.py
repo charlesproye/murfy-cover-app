3e6f396e0fdbd63521603395ed23cbb95ba2ffff
Charles Proye <charles.proye@gmail.com>
2025-07-23 16:10:19 +0200

    Cleaning avant push

--- src/transform/processed_tss/raw_ts_to_processed_ts.py
+++ src/transform/processed_tss/raw_ts_to_processed_ts.py
@@ -18,7 +18,8 @@ from core.spark_utils import (get_optimal_nb_partitions,
                               safe_astype_spark_with_error_handling)
 from transform.fleet_info.main import fleet_info
 from transform.processed_tss.config import *
-from transform.processed_tss.config import NB_CORES_CLUSTER, SCALE_SOC
+from transform.processed_tss.config import (NB_CORES_CLUSTER, SCALE_SOC,
+                                            SOC_DIFF_THRESHOLD)
 from transform.raw_tss.main import *
 
 
@@ -53,13 +54,10 @@ class RawTsToProcessedTs(CachedETLSpark):
     # TODO:
     # Keep overwrite mode as long as performance is acceptable, don't switch to 'append' yet
     # Keep non-batched system as long as memory usage is manageable
-    # Trier ce qui est utile ou non dans la config
     # Clean cette classe
-    # Faire tourner sur tout
 
     def run(self):
 
-        # Check if raw_ts exists, if not, run ResponseToRawTs
         if self.bucket.check_spark_file_exists(
             f"raw_ts/{self.make}/time_series/raw_ts_spark.parquet"
         ):
@@ -81,10 +79,12 @@ class RawTsToProcessedTs(CachedETLSpark):
         tss = tss.select(*NECESSARY_COLS[self.make])  # Reduce column volumetry
 
         tss = safe_astype_spark_with_error_handling(tss)
-        tss = self.normalize_units_to_metric(tss)
+        tss = self._normalize_units_to_metric(tss)
         tss = tss.orderBy(["vin", "date"])
 
-        tss = self.compute_charge_idx(tss)  # Charging Status Indew
+        tss = self._compute_charge_idx(
+            tss, SOC_DIFF_THRESHOLD[self.make]
+        )  # Charging Status Index
 
         tss = self.compute_specific_features(tss)
 
@@ -94,11 +94,7 @@ class RawTsToProcessedTs(CachedETLSpark):
 
         return tss
 
-    def _set_optimal_spark_parameters(
-        self,
-        tss,
-        nb_cores: int = 8,
-    ):
+    def _set_optimal_spark_parameters(self, tss, nb_cores: int = 8):
         """
         Calculates the optimal batch size for parallel processing of VINs.
 
@@ -124,10 +120,6 @@ class RawTsToProcessedTs(CachedETLSpark):
 
         nb_vins = tss.select(count_distinct("vin")).collect()[0][0]
 
-        print("Nb vins: ", nb_vins)
-
-        print(nb_vins)
-
         if nb_vins == 0:
             self.logger.warning("No VINs to process, returning batch_size = 1")
             return (1, nb_vins)
@@ -140,7 +132,7 @@ class RawTsToProcessedTs(CachedETLSpark):
 
         return (4 * nb_cores, vin_per_batch)
 
-    def normalize_units_to_metric(self, tss):
+    def _normalize_units_to_metric(self, tss):
         tss = tss.withColumn("odometer", col("odometer") * 1.609)
         return tss
 
@@ -157,13 +149,11 @@ class RawTsToProcessedTs(CachedETLSpark):
             DataFrame: DataFrame avec la colonne `phase_id` mise à jour
         """
 
-        # 1. Marquer les phases valides
         df = df.withColumn(
             "is_valid_phase",
             F.when(F.col("total_phase_time") >= min_duration_minutes, 1).otherwise(0),
         )
 
-        # 2. Déterminer la dernière phase valide précédemment
         w_time = (
             Window.partitionBy("vin")
             .orderBy("date")
@@ -178,7 +168,6 @@ class RawTsToProcessedTs(CachedETLSpark):
             ).over(w_time),
         )
 
-        # 3. Mettre à jour le phase_id
         df = df.withColumn(
             "phase_id_updated",
             F.when(F.col("is_valid_phase") == 1, F.col("phase_id")).otherwise(
@@ -186,14 +175,12 @@ class RawTsToProcessedTs(CachedETLSpark):
             ),
         )
 
-        # 4. Re-numérotation des phase_id pour compacter (optionnel mais propre)
         df = df.withColumn(
             "phase_id_final",
             F.dense_rank().over(Window.partitionBy("vin").orderBy("phase_id_updated"))
             - 1,
         )
 
-        # 5. Nettoyage final
         df = df.drop(
             "phase_id", "last_valid_phase_id", "is_valid_phase", "phase_id_updated"
         )
@@ -201,12 +188,14 @@ class RawTsToProcessedTs(CachedETLSpark):
 
         return df
 
-    def compute_charge_idx(self, tss: DF) -> DF:
+    def _compute_charge_idx(
+        self, tss: DF, total_soc_diff_threshold: float = 0.05
+    ) -> DF:
 
-        # Définir les fenêtres
         w = Window.partitionBy("vin").orderBy("date")
 
-        # Calculer soc_diff en allant chercher la précédente valeur non nulle
+        tss = tss.withColumn("soc", F.col("soc") * SCALE_SOC[self.make])
+
         tss = tss.withColumn(
             "soc_diff",
             F.when(
@@ -217,20 +206,18 @@ class RawTsToProcessedTs(CachedETLSpark):
                 ),
             ).otherwise(None),
         )
-        # Calcul du gap en minutes
+
         df = tss.withColumn("prev_date", lag("date").over(w))
         df = df.withColumn(
             "time_gap_minutes",
             (F.unix_timestamp("date") - F.unix_timestamp("prev_date")) / 60,
         )
 
-        # Calcul de direction avec forward fill
         df = df.withColumn(
             "direction_raw",
             F.when(col("soc_diff").isNull(), None).otherwise(F.signum("soc_diff")),
         )
 
-        # Forward fill de la direction
         df = df.withColumn(
             "direction",
             F.last("direction_raw", ignorenulls=True).over(
@@ -238,15 +225,13 @@ class RawTsToProcessedTs(CachedETLSpark):
                 .orderBy("date")
                 .rowsBetween(Window.unboundedPreceding, 0)
             ),
-        )
+        )  # Direction forward fill
 
-        # Détecter les changements de direction
         df = df.withColumn(
             "direction_change",
             F.when(F.col("direction") != F.lag("direction").over(w), 1).otherwise(0),
         )
 
-        # Créer phase_id en cumulant les changements
         df = df.withColumn(
             "phase_id",
             F.sum("direction_change").over(w.rowsBetween(Window.unboundedPreceding, 0)),
@@ -256,7 +241,9 @@ class RawTsToProcessedTs(CachedETLSpark):
 
         df = df.withColumn("total_phase_time", F.sum("time_gap_minutes").over(w_phase))
 
-        df = self._reassign_short_phases(df)
+        df = self._reassign_short_phases(
+            df
+        )  # Reassign short phases to previous valid phase (especiallyuseful for tesla-fleet-telemetry noise)
 
         w_phase = Window.partitionBy("vin", "phase_id")
 
@@ -268,10 +255,8 @@ class RawTsToProcessedTs(CachedETLSpark):
 
         df = df.withColumn(
             "charging_status",
-            F.when(F.col("total_soc_diff") > 0.005 * SCALE_SOC[self.make], "charging")
-            .when(
-                F.col("total_soc_diff") < -0.005 * SCALE_SOC[self.make], "discharging"
-            )
+            F.when(F.col("total_soc_diff") > total_soc_diff_threshold, "charging")
+            .when(F.col("total_soc_diff") < -total_soc_diff_threshold, "discharging")
             .when(
                 (F.col("prev_phase") == F.col("next_phase"))
                 & (F.col("prev_phase") >= 0),
@@ -284,7 +269,7 @@ class RawTsToProcessedTs(CachedETLSpark):
             )
             .otherwise("idle"),
         )
-        # Étape 3: Recréer le phase_id basé sur les changements de phase
+
         df = df.withColumn(
             "charging_status_change",
             F.when(
