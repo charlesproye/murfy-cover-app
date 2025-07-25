6e24d52925b58db822badea7439eeac9cf79dd90
Charles Proye <charles.proye@gmail.com>
2025-07-25 14:13:17 +0200

    Premier commit d'uniformisation de la partie compress avec tests par OEMs

--- src/transform/compressor/providers/volkswagen.py
+++ src/transform/compressor/providers/volkswagen.py
@@ -5,9 +5,14 @@ from src.core.compressor import Compressor
 
 
 class VolkswagenCompressor(Compressor):
+    def __init__(self, make='volkswagen') -> None:
+        super().__init__()
+        self.make = make
+        self._s3 = AsyncS3()
+
     @property
     def brand_prefix(self) -> str:
-        return "volkswagen"
+        return self.make
 
     def _temp_data_to_daily_file(self, new_files: dict[str, bytes]) -> bytes:
         data = []
@@ -18,11 +23,10 @@ class VolkswagenCompressor(Compressor):
         return msgspec.json.encode({"data": data})
 
 
-async def compress():
-    compressor = VolkswagenCompressor()
-    await compressor.run()
-
-
-if __name__ == "__main__":
-    asyncio.run(compress())
+    @classmethod
+    async def compress(cls, make):
+        """Compress data for any make"""
+        asyncio.get_event_loop().set_debug(False)
+        compressor = cls(make)
+        await compressor.run()
 
