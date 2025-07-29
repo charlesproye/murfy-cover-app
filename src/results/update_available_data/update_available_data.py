36cccdf0ba917d2a7457f3b42edaa614f45fc16c
hugochant <hugo.c@club-internet.fr>
2025-07-29 16:37:42 +0200

    script to check data available

--- src/results/update_available_data/update_available_data.py
+++ src/results/update_available_data/update_available_data.py
@@ -1,69 +1,24 @@
-from core.sql_utils import get_sqlalchemy_engine
-from core.pandas_utils import DF
-import pandas as pd
-from core.spark_utils import create_spark_session
-from core.s3.s3_utils import S3Service
-from core.s3.settings import S3Settings
-from .config import MAKES, TABLE_QUERY
-from core.console_utils import main_decorator
 from sqlalchemy.engine import Engine
-from pyspark.sql import functions as F
 from sqlalchemy import text
-import logging
+from core.sql_utils import get_sqlalchemy_engine
+from core.console_utils import main_decorator
 
 
+COLUMNS_TO_CHECK = ['odometer', 'soh', 'soh_oem']
 
+def check_columns_from_model(model_id, column_list, engine):
+    """Returns a list of column names from vehicle_data that contain at least one non-null value
+    for vehicles associated with a specific vehicle model.
 
+    Parameters:
+        model_id (int): The ID of the vehicle model to filter on (vehicle_model.id).
+        column_list (list of str): The list of column names (from vehicle_data) to check.
+        engine (sqlalchemy.Engine): A SQLAlchemy engine instance used to execute the query.
 
-def get_fleet_info(con: Engine) -> DF:
-    fleet_info:DF = (
-        pd.read_sql_query(TABLE_QUERY, con)
-    )
-
-    fleet_info_spark = spark.createDataFrame(fleet_info)
-
-    return fleet_info_spark
-
-def get_presence_per_type(tss: DF, fleet_info_spark: DF, con: Engine, column_to_check: str, column_to_update: str):
-
-    if '.' in column_to_check:
-        tss = tss.withColumnsRenamed({column_to_check: column_to_check.replace('.', '_')})
-        column_to_check = column_to_check.replace('.', '_')
-
-    merged_df = fleet_info_spark.join(tss, "vin", "left")
-
-    presence_check_df = (
-        merged_df
-        .groupBy("model_name", "type")
-        .agg(
-            F.count(F.when(F.col(f"{column_to_check}").isNotNull(), 1)).alias(f"non_null_{column_to_check}"),
-        )
-        .orderBy("model_name", "type")
-    ).filter(F.col(f"non_null_{column_to_check}") > 0)
-
-    df_pd = presence_check_df.coalesce(32).toPandas()
-
-    condition = ""
-    for model, version in zip(df_pd['model_name'], df_pd['type']): 
-        condition += f"('{model}', '{version}'),"
-    
-    print('Before query update...')
-    query = f"""
-    UPDATE vehicle_model
-    SET {column_to_update} = TRUE
-    WHERE (model_name, type) IN (
-    {condition[:-1]}
-    );
-    """
-
-    with con.begin() as connection:
-        print(query)
-        connection.execute(text(query)) 
-        connection.close()
-
-    return query
-
-def check_columns_from_model(model_id, column_list, is_prod=False):
+    Returns:
+        list of str: Column names from `column_list` that have at least one non-null value
+                     for vehicles matching the given model_id.
+                     """
     query_parts = []
     params = {}
 
@@ -83,37 +38,57 @@ def check_columns_from_model(model_id, column_list, is_prod=False):
         query_parts.append(part.strip())
 
     full_query = "\nUNION ALL\n".join(query_parts)
-
-    engine = get_sqlalchemy_engine(is_prod)
     with engine.begin() as conn:
         result = conn.execute(text(full_query), params)
         return [row[0] for row in result.fetchall()]
 
+def update_model_columns(model_id, available_columns: list, all_columns: list, engine: Engine):
+    """
+    Updates the boolean columns of vehicle_model with TRUE/FALSE
+    based on their presence in available_columns.
+
+    Args:
+        model_id (str): The ID of the model to update.
+        available_columns (List[str]): List of columns that are present.
+        all_columns (List[str]): Complete list of boolean columns to update.
+        engine (sqlalchemy.Engine): SQLAlchemy engine instance.
+    """
+    update_clauses = []
+    params = {"model_id": model_id}
+
+    for col in all_columns:
+        param_name = f"{col}_value"
+        update_clauses.append(f"{col}_data = :{param_name}")
+        params[param_name] = col in available_columns
+
+    update_sql = f"""
+        UPDATE vehicle_model
+        SET {', '.join(update_clauses)}
+        WHERE id = :model_id
+    """
+
+    with engine.begin() as conn:
+        conn.execute(text(update_sql), params)
+
 
 @main_decorator
-def run(spark, s3: S3Service, con: Engine):
-    fleet_info = get_fleet_info(con)
-    for make, dic_cols in MAKES.items():
-        tss = s3.read_parquet_df_spark(spark, f"raw_ts/{make}/time_series/raw_ts_spark.parquet")
-        for column_to_update, column_to_check in dic_cols.items():
-            get_presence_per_type(tss, fleet_info, con, column_to_check, column_to_update)
+def run(engine: Engine):
+    with engine.begin() as conn:
+        result = conn.execute(text("SELECT id FROM vehicle_model where type in ('r110') "))
+        model_ids = [row[0] for row in result.fetchall()]
 
-
-
+    for model_id in model_ids:
+        print(model_id)
+        try:
+            print(f"Checking {model_id}...")
+            available_columns = check_columns_from_model(model_id, COLUMNS_TO_CHECK, engine)
+            print('available_columns =', available_columns)
+            update_model_columns(model_id, available_columns, COLUMNS_TO_CHECK, engine)
+            print(f"✅ {model_id} updated with : {available_columns}")
+        except Exception as e:
+            print(f"❌ Error for {model_id} : {e}")
 
 if __name__ == "__main__":
     con = get_sqlalchemy_engine()
-
-    s3 = S3Service()
-
-    spark = create_spark_session(
-        S3Settings().S3_KEY,
-        S3Settings().S3_SECRET
-    )
-
-    run(spark, s3, con)
-
-
-
-
+    run(con)
 
