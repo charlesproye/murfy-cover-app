c77b5a6e3cb7a69b86b734565df011fdc070d0d0
hugochant <hugo.c@club-internet.fr>
2025-09-22 09:28:03 +0200

    add model in main results

--- src/results/main.py
+++ src/results/main.py
@@ -1,9 +1,8 @@
-from src.results.scoring.update_scoring import main as update_scoring
-from src.results.soh_scrapping.scrapping_aramis import main as scrapping_aramis
-from src.results.soh_scrapping.scrapping_autospherre import main as scrapping_autospherre
-from src.results.trendlines_results.trendlines import run_trendline_main
-from src.results.trendlines_results.trendlines_excel import run_excel_trendlines
-from src.core.gsheet_utils import clean_gsheet
+from results.scoring.update_scoring import main as update_scoring
+from results.soh_scrapping.scrapping_aramis import main as scrapping_aramis
+from results.soh_scrapping.scrapping_autospherre import main as scrapping_autospherre
+from core.gsheet_utils import clean_gsheet
+from results.forecast.price_predictor import CarPricePredictor
 
 if __name__ == "__main__":
     #Front 
@@ -15,4 +14,7 @@ if __name__ == "__main__":
     # #Calculate Trendlines
     # run_trendline_main()
     # run_excel_trendlines()
+    # price forecast
+    model = CarPricePredictor()
+    model.run('price_model.pickle')
 
