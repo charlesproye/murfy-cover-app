e3afac7b042ef2001415e61cffde4c498563f708
Charles Proye <charles.proye@gmail.com>
2025-10-27 16:13:26 +0000

    Merged in DATAEV-616-organiser-les-namespaces-k8s- (pull request #330)
    Migration vers les namespaces evalue & data engineering pour docker et k8s
    
    * Migration vers les namespaces evalue & data engineering pour docker et k8s
    
    
    Approved-by: Robin Fourcade

--- web/deploy_fapi.sh
+++ web/deploy_fapi.sh
@@ -1,16 +1,17 @@
 # Authenticate with Scaleway CLI (first time only)
 # scw init
 
-# create image 
+# create image
 docker compose -f ../docker-compose.yaml build ingestion-web-app
 
-# login first time only 
-# docker login rg.fr-par.scw.cloud/bib-images -u nologin --password-stdin <<< "$SCW_SECRET_KEY"
+# login first time only
+# docker login rg.fr-par.scw.cloud/data-engineering -u nologin --password-stdin <<< "$SCW_SECRET_KEY"
 
-# tag to the good format 
-docker tag ingestion-web-app:latest rg.fr-par.scw.cloud/bib-images/ingestion-web-app:latest
+# tag to the good format
+docker tag ingestion-web-app:latest rg.fr-par.scw.cloud/data-engineering/ingestion-web-app:latest
 
 # push image to registry
-docker push rg.fr-par.scw.cloud/bib-images/ingestion-web-app:latest
+docker push rg.fr-par.scw.cloud/data-engineering/ingestion-web-app:latest
 
 # then go to the console and redeploy the container
+
