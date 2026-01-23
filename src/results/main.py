203e53cabf8749c832b6a7caecf21791fb929d38
Charles <charles.proye@bib-batteries.fr>
2026-01-23 09:16:39 +0100

    fix pipeline

--- src/results/main.py
+++ src/results/main.py
@@ -3,7 +3,7 @@ import logging
 from results.forecast.price_predictor import train_and_save
 from results.real_autonomy.calculate_real_autonomy import update_real_autonomy
 from results.scoring.update_scoring import compute_bib_score
-from results.trendline.main import update_trendline_model, update_trendline_oem
+from results.trendline.main import main as update_trendlines
 from results.update_available_data.main import update_available_data
 from results.vehicle_status.main import run_vehicle_status_checks
 
@@ -13,8 +13,7 @@ LOGGER = logging.getLogger(__name__)
 def results_pipeline(logger: logging.Logger = LOGGER):
     available_data_summary = update_available_data(logger=logger) or {}
     scoring_summary = compute_bib_score(logger=logger) or {}
-    trendline_oem_summary = update_trendline_oem() or {}
-    trendline_model_summary = update_trendline_model() or {}
+    trendline_model_summary = update_trendlines() or {}
     price_forecast_summary = train_and_save("model_price.pkl", logger=logger) or {}
     vehicle_status_summary = run_vehicle_status_checks(logger=logger) or {}
     real_autonomy_summary = update_real_autonomy() or {}
@@ -22,7 +21,6 @@ def results_pipeline(logger: logging.Logger = LOGGER):
     return {
         "available_data": available_data_summary,
         "scoring": scoring_summary,
-        "trendline_oem": trendline_oem_summary,
         "trendline_model": trendline_model_summary,
         "price_forecast": price_forecast_summary,
         "vehicle_status": vehicle_status_summary,
